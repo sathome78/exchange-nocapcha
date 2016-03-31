35667f816b8880e7976b4c149b6afe43cd80b9e2
Denis Savin <pilgrimm333@gmail.com>
2016-03-31 17:28:31 +0300

    Removed unnecessary system property setter

--- Controller/src/main/java/me/exrates/config/SpringMvcInitializer.java
+++ Controller/src/main/java/me/exrates/config/SpringMvcInitializer.java
@@ -1,17 +1,10 @@
 package me.exrates.config;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 
 
 public class SpringMvcInitializer 
        extends AbstractAnnotationConfigDispatcherServletInitializer {
-
-	private static final Logger logger = LogManager.getLogger(SpringMvcInitializer.class);
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
@@ -26,16 +19,5 @@ public class SpringMvcInitializer
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
-	}
-
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		super.onStartup(servletContext);
-		String activeProfile = System.getProperty("profile_active");
-		if (activeProfile == null) {
-			activeProfile = "dev";
-		}
-		logger.info("Active profile :" + activeProfile);
-		servletContext.setInitParameter("spring.profile.active" , activeProfile);
 	}
 }
