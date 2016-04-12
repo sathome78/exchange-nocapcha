27336807ccdd2721597f3bb3512c8783c6d2f2e2
ValkSam <do110473sva@gmail.com>
2016-04-12 13:57:20 +0300

    TESTING-24-22 added currency pair switcher and ordering pairs

--- Dao/src/main/resources/patch_27_added_pair_order_field_to_currency_pair.sql
+++ Dao/src/main/resources/patch_27_added_pair_order_field_to_currency_pair.sql
@@ -9,3 +9,5 @@ SET `pair_order` = 2 WHERE `name` = "EDRC/USD";
 
 UPDATE currency_pair
 SET `pair_order` = 3 WHERE `name` = "EDRC/RUR";
+
+INSERT INTO DATABASE_PATCH VALUES('patch_27_added_pair_order_field_to_currency_pair',default,1);
