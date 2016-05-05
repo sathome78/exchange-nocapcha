4db8d8541dca4bfef771cd10a04e93a201403546
ValkSam <do110473sva@gmail.com>
2016-05-05 12:01:52 +0300

    new ip confirm + refactoring the registration according to  ip confirmation + refactoring orders block for protect from hack changing data on site + adminka: wallets summary and upload users wallets data

--- Controller/src/main/webapp/client/js/upload.js
+++ Controller/src/main/webapp/client/js/upload.js
@@ -2,7 +2,7 @@
  * Created by Valk on 05.05.2016.
  */
 
-$(function(){
+$(function () {
     $('#upload-users-wallets').on('click', uploadUsersWalletsSummary);
 });
 
@@ -11,7 +11,14 @@ function uploadUsersWalletsSummary() {
             url: '/admin/uploadUsersWalletsSummary',
             type: 'GET',
             success: function (data) {
-                $('<a href="data:text/plain,%EF%BB%BF' + encodeURIComponent(data) + '" download="uploadUsersWalletsSummary.csv"/a>')[0].click();
+                /* not works in FF
+                $('<a href="data:text/plain,%EF%BB%BF' + encodeURIComponent(data) + '" download="uploadUsersWalletsSummary.csv"/a>')[0].click();*/
+                var link = document.createElement('a');
+                link.href = "data:text/plain;charset=utf-8,%EF%BB%BF" + encodeURIComponent(data);
+                link.download = "uploadUsersWalletsSummary.csv";
+                var e = document.createEvent('MouseEvents');
+                e.initEvent('click', true, true);
+                link.dispatchEvent(e);
             }
         }
     );
