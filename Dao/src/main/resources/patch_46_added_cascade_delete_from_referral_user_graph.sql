c3101e5136b4ffe80ccbfad21c11fd47dd2d035b
Denis Savin <denis.savin.x@gmail.com>
2016-06-16 15:41:17 +0300

    Small fix for DB patch 46

--- Dao/src/main/resources/patch_46_added_cascade_delete_from_referral_user_graph.sql
+++ Dao/src/main/resources/patch_46_added_cascade_delete_from_referral_user_graph.sql
@@ -2,3 +2,5 @@ ALTER TABLE birzha.REFERRAL_USER_GRAPH DROP FOREIGN KEY referral_user_graph_ibfk
 ALTER TABLE birzha.REFERRAL_USER_GRAPH
   ADD CONSTRAINT referral_user_graph_ibfk_1
 FOREIGN KEY (child) REFERENCES USER (id) ON DELETE CASCADE ON UPDATE CASCADE;
+
+INSERT INTO DATABASE_PATCH VALUES('patch_46_added_cascade_delete_from_referral_user_graphп',default,1);
