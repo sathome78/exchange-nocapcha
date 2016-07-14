32249aa82bfc814891b211d35dc254ca05b6bd9c
ajet21 <ajet5911@gmail.com>
2016-07-15 01:11:08 +0300

    fix

--- Dao/src/main/resources/patch_53_added_imageMerchantId_column_in_withdraw.sql
+++ Dao/src/main/resources/patch_53_added_imageMerchantId_column_in_withdraw.sql
@@ -1,10 +1,10 @@
 ALTER TABLE `birzha`.`WITHDRAW_REQUEST`
 ADD COLUMN `merchant_image_id` INT(11) NULL DEFAULT NULL AFTER `processed_by`,
 ADD INDEX `withdraw_request_ibfk_3_idx` (`merchant_image_id` ASC);
-ALTER TABLE `birzha`.`withdraw_request`
+ALTER TABLE `birzha`.`WITHDRAW_REQUEST`
 ADD CONSTRAINT `withdraw_request_ibfk_3`
   FOREIGN KEY (`merchant_image_id`)
-  REFERENCES `birzha`.`merchant_image` (`id`)
+  REFERENCES `birzha`.`MERCHANT_IMAGE` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
