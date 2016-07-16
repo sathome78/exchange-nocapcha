60b30d76c161acadfcd889eb222e9143ccbd7564
ValkSam <do110473sva@gmail.com>
2016-07-17 01:04:36 +0300

    in work..

--- Controller/src/main/java/me/exrates/controller/advice/GlobalControllerExceptionHandler.java
+++ Controller/src/main/java/me/exrates/controller/advice/GlobalControllerExceptionHandler.java
@@ -1,18 +1,9 @@
 package me.exrates.controller.advice;
 
-import me.exrates.model.UserFile;
 import me.exrates.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.multipart.MultipartException;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
-import java.util.Locale;
 
 /**
  * @author Denis Savin (pilgrimm333@gmail.com)
