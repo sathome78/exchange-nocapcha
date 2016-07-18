20f234f6b5db7431165775c630fa6657cec7ad96
Denis Savin <denis.savin.x@gmail.com>
2016-07-18 15:04:42 +0300

    Merge commit

--- Controller/src/main/java/me/exrates/controller/advice/GlobalControllerExceptionHandler.java
+++ Controller/src/main/java/me/exrates/controller/advice/GlobalControllerExceptionHandler.java
@@ -1,10 +1,18 @@
 package me.exrates.controller.advice;
 
+import me.exrates.model.UserFile;
 import me.exrates.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+import java.util.Locale;
 /**
  * @author Denis Savin (pilgrimm333@gmail.com)
  */
@@ -20,15 +28,15 @@ public class GlobalControllerExceptionHandler {
         this.userService = userService;
     }
 
-    /*@ExceptionHandler(MultipartException.class)
+    @ExceptionHandler(MultipartException.class)
     public ModelAndView handleMultipartConflict(final Locale locale) {
         final User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         final me.exrates.model.User user = userService.findByEmail(principal.getUsername());
-        final ModelAndView mav = new ModelAndView("settings");
+        final ModelAndView mav = new ModelAndView("globalPages/settings");
         final List<UserFile> userFiles = userService.findUserDoc(user.getId());
         mav.addObject("user", user);
         mav.addObject("errorNoty", messageSource.getMessage("admin.errorUploadFiles", null, locale));
         mav.addObject("userFiles", userFiles);
         return mav;
-    }*/
+    }
 }
