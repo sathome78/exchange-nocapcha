6833dea08d89c22f6c9f48ae9b0b876fd962091c
wayfarer_88 <golvazin@gmail.com>
2016-07-27 10:28:09 +0300

    transaction invoice table - add columns and apply new design

--- Service/src/main/java/me/exrates/service/impl/InvoiceServiceImpl.java
+++ Service/src/main/java/me/exrates/service/impl/InvoiceServiceImpl.java
@@ -1,6 +1,8 @@
 package me.exrates.service.impl;
 
+import me.exrates.dao.InvoiceRequestDao;
 import me.exrates.model.CreditsOperation;
+import me.exrates.model.InvoiceRequest;
 import me.exrates.model.Transaction;
 import me.exrates.service.InvoiceService;
 import me.exrates.service.TransactionService;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Set;
 
 
 @Service
@@ -19,6 +23,9 @@ public class InvoiceServiceImpl implements InvoiceService {
     @Autowired
     private TransactionService transactionService;
 
+    @Autowired
+    private InvoiceRequestDao invoiceRequestDao;
+
     private static final Logger LOG = LogManager.getLogger("merchant");
 
     @Override
@@ -26,17 +33,26 @@ public class InvoiceServiceImpl implements InvoiceService {
     public Transaction createPaymentInvoice(final CreditsOperation creditsOperation) {
 
         final Transaction transaction = transactionService.createTransactionRequest(creditsOperation);
+        InvoiceRequest invoiceRequest = new InvoiceRequest();
+        invoiceRequest.setTransaction(transaction);
+        invoiceRequest.setUserEmail(creditsOperation.getUser().getEmail());
+        System.out.println(invoiceRequest);
+        invoiceRequestDao.create(invoiceRequest, creditsOperation.getUser());
+
 
         return transaction;
     }
 
     @Override
     @Transactional
-    public boolean provideTransaction(int id) {
+    public boolean provideTransaction(int id, String acceptanceUserEmail) {
 
         Transaction transaction = transactionService.findById(id);
         try {
             transactionService.provideTransaction(transaction);
+            InvoiceRequest invoiceRequest = invoiceRequestDao.findById(id).get();
+            invoiceRequest.setAcceptanceUserEmail(acceptanceUserEmail);
+            invoiceRequestDao.setAcceptance(invoiceRequest);
         }catch (Exception e){
             LOG.error(e);
             return false;
@@ -44,4 +60,8 @@ public class InvoiceServiceImpl implements InvoiceService {
        return true;
     }
 
+    @Override
+    public List<InvoiceRequest> findAllInvoiceRequests() {
+        return invoiceRequestDao.findAll();
+    }
 }
