6d0dfcf2c9d8cde5f5975ae8e75deed515e375c6
wayfarer_88 <golvazin@gmail.com>
2016-08-08 19:50:18 +0300

    add sql patch

---
+++ Dao/src/main/resources/patch_56_add_banned_in_chat_user_status.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER_STATUS(id, name, description) values (4, 'banned_in_chat', 'banned in chat by admin');
+INSERT INTO DATABASE_PATCH VALUES('patch_55_add_invoice_requests',default,1);
