a317fc72bded2f94d35272aeb5c08fe94f1262ba
golvazin <wayfarer2006@ukr.net>
2016-08-16 12:12:02 +0300

    test

--- Controller/src/main/java/me/exrates/controller/filter/EncodingFilter.java
+++ Controller/src/main/java/me/exrates/controller/filter/EncodingFilter.java
@@ -21,6 +21,7 @@ public class EncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+        System.out.println("talala");
         LOGGER.debug(request.getCharacterEncoding());
         chain.doFilter(request, response);
 
