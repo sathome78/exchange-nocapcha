bb674c9f30b7f20581623f092e263530702498e3
golvazin <wayfarer2006@ukr.net>
2016-09-22 15:38:17 +0300

    fix

--- Controller/src/main/webapp/client/js/main.js
+++ Controller/src/main/webapp/client/js/main.js
@@ -609,3 +609,25 @@ $(function(){
     });
 
 });
+
+function parseNumber(numberStr) {
+    /*ATTENTION: this func wil by work correctly if number always has decimal separator
+     * for this reason we use BigDecimalProcessing.formatLocale(rs.getBigDecimal("some value"), locale, 2)
+     * which makes 1000.00 from 1000 or 1000.00000
+     * or we can use igDecimalProcessing.formatLocale(rs.getBigDecimal("some value"), locale, true)*/
+    if (numberStr.search(/\,.*\..*/) != -1) {
+        /*100,000.12 -> 100000.12*/
+        numberStr = numberStr.replace(/\,/g, '');
+    } else if (numberStr.search(/\..*\,.*/) != -1) {
+        /*100.000,12 -> 100000.12*/
+        numberStr = numberStr.replace(/\./g, '').replace(/\,/g, '.');
+    } else if (numberStr.search(/\s.*\..*/) != -1) {
+        /*100 000.12 -> 100000.12*/
+        numberStr = numberStr.replace(/\s/g, '');
+    } else if (numberStr.search(/\s.*\,.*/) != -1) {
+        /*100 000,12 -> 100000.12*/
+        numberStr = numberStr.replace(/\s/g, '').replace(/\,/g, '.');
+    }
+    numberStr = numberStr.replace(/\s/g, '').replace(/\,/g, '.');
+    return parseFloat(numberStr);
+}
