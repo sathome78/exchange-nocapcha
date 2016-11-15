42795c8296fe64ca870310c019332a23003a2d34
golvazin <wayfarer2006@ukr.net>
2016-11-15 17:22:48 +0200

    fix

--- Dao/src/main/resources/patch_75_add_notifications.sql
+++ Dao/src/main/resources/patch_75_add_notifications.sql
@@ -28,4 +28,18 @@ INSERT INTO NOTIFICATION_EVENT VALUES (3, 'ACCOUNT', NULL);
 INSERT INTO NOTIFICATION_EVENT VALUES (4, 'ORDER', NULL);
 INSERT INTO NOTIFICATION_EVENT VALUES (5, 'IN_OUT', NULL);
 
-INSERT INTO DATABASE_PATCH (version, datetime, patched) VALUES ('patch_75_add_notifications', DEFAULT, 1);
+CREATE TABLE `NOTIFICATION_OPTIONS` (
+  `notification_event_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `send_notification` tinyint(1) NOT NULL DEFAULT '1',
+  `send_email` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`notification_event_id`,`user_id`),
+  KEY `user_id_idx` (`user_id`),
+  CONSTRAINT `fk_notification_event_id1` FOREIGN KEY (`notification_event_id`) REFERENCES `NOTIFICATION_EVENT` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_id1` FOREIGN KEY (`user_id`) REFERENCES `USER` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+INSERT INTO NOTIFICATION_OPTIONS (notification_event_id, user_id)
+  SELECT NOTIFICATION_EVENT.id AS ntf_id, USER.id AS usr_id FROM NOTIFICATION_EVENT JOIN USER;
+
+  INSERT INTO DATABASE_PATCH (version, datetime, patched) VALUES ('patch_75_add_notifications', DEFAULT, 1);
