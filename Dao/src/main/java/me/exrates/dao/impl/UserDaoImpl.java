fba788929c410948ca1f174156c25709cf2cee85
golvazin <wayfarer2006@ukr.net>
2016-12-19 19:12:06 +0200

    fix

--- Dao/src/main/java/me/exrates/dao/impl/UserDaoImpl.java
+++ Dao/src/main/java/me/exrates/dao/impl/UserDaoImpl.java
@@ -83,7 +83,7 @@ public class UserDaoImpl implements UserDao {
         String sqlUser = "insert into USER(nickname,email,password,phone,status,roleid ) " +
                 "values(:nickname,:email,:password,:phone,:status,:roleid)";
         String sqlWallet = "INSERT INTO WALLET (currency_id, user_id) select id, :user_id from CURRENCY where name != 'LTC';";
-        String sqlNotificationOptions = "INSERT INTO NOTIFICATION_OPTIONS(notification_event_id, user_id) select id, :user_id FROM notification_event; ";
+        String sqlNotificationOptions = "INSERT INTO NOTIFICATION_OPTIONS(notification_event_id, user_id) select id, :user_id FROM NOTIFICATION_EVENT; ";
         Map<String, String> namedParameters = new HashMap<String, String>();
         namedParameters.put("email", user.getEmail());
         namedParameters.put("nickname", user.getNickname());
