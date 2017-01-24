f30a5c1b10838926d379c19afb9c76a4b0ec3d36
golvazin <wayfarer2006@ukr.net>
2017-01-24 11:38:00 +0200

    fix

--- Dao/src/main/resources/patch_83_hide_wallets.sql
+++ Dao/src/main/resources/patch_83_hide_wallets.sql
@@ -2,3 +2,4 @@ ALTER TABLE CURRENCY
   ADD COLUMN `hidden` TINYINT(1) NULL DEFAULT 0 AFTER `min_withdraw_sum`;
 
 UPDATE CURRENCY SET hidden = 1 WHERE name IN ('LTC', 'CNY', 'IDR', 'THB', 'NGN');
+INSERT INTO DATABASE_PATCH VALUES ('patch_83_hide_wallets', default, 1);
