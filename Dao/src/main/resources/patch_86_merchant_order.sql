816166bffca367c3a785bf0fa75acd36b86bc6bf
ajet21 <ajet5911@gmail.com>
2017-01-30 23:34:11 +0200

    add merchant order

--- Dao/src/main/resources/patch_86_merchant_order.sql
+++ Dao/src/main/resources/patch_86_merchant_order.sql
@@ -2,4 +2,4 @@
 ALTER TABLE `birzha`.`MERCHANT`
 ADD COLUMN `merchant_order` INT(2) NULL DEFAULT NULL AFTER `name`;
 
-INSERT INTO DATABASE_PATCH VALUES('patch_83_added_okpay_support',default,1);
+INSERT INTO DATABASE_PATCH VALUES('patch_86_merchant_order',default,1);
