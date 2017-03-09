c0f6f3b4c7bbf58f666c97ca58b5b31710d9ceca
golvazin <wayfarer2006@ukr.net>
2017-03-09 15:58:47 +0200

    fix

--- Controller/src/main/webapp/client/js/dataTable/adminCandleDataTable.js
+++ Controller/src/main/webapp/client/js/dataTable/adminCandleDataTable.js
@@ -27,14 +27,15 @@ $(function () {
         lang:'ru',
         defaultDate: new Date(),
         defaultTime: '00:00',
-        value: moment().subtract(1, 'days').format('YYYY-MM-DD HH:mm:ss')
+        value: moment().subtract(1, 'days').format('YYYY-MM-DD HH:mm:ss'),
+        onChangeDateTime: function(current_time, $input){
+            console.log($filterForm.serialize());
+        }
     });
 
-    console.log($filterForm.serialize());
+    $('#currencyPair, #interval').on('change', updateCandleTable());
 
-    $.get('/2a8fy7b07dxe44/getCandleTableData?' + $filterForm.serialize(), function (data) {
-        console.log(data);
-    });
+
 
     function updateCandleTable() {
         var url = '/2a8fy7b07dxe44/getCandleTableData?' + $filterForm.serialize();
@@ -89,49 +90,12 @@ $(function () {
                         "render": function (data) {
                             return numeral(data).format('0.00[000000]');
                         }
-                    },
-                    {
-                        "data": "acceptanceTime",
-                        "render": function (data) {
-                            return data ? data.replace(' ', '<br/>') : '-';
-                        },
-                        "className": "text-center"
-                    },
-                    {
-                        "data": "hash",
-                        "render": function (data, type, row) {
-                            var readonly = data == null ? '' : 'readonly';
-                            var inputValue = data == null ? '' : data;
-                            return '<input id="bitcoin_hash' + row.invoiceId +'" ' + readonly + ' value="' + inputValue + '" style="width: 130px" ' +
-                                'class="form-control input-block-wrapper__input">';
-                        }
-                    },
-                    {
-                        "data": "hash",
-                        "render": function (data, type, row) {
-                            var readonly = data == null ? '' : 'readonly';
-                            var totalAmount = row.amount + row.commissionAmount;
-                            return '<input id="manual_amount'+ row.invoiceId +'" ' + readonly + ' value="' + numeral(totalAmount).format('0.00000000') + '" style="width: 130px"  maxlength="9" ' +
-                                'class="form-control input-block-wrapper__input numericInputField">';
-                        }
-                    },
-                    {
-                        "data": "confirmation"
-                    },
-                    {
-                        "data": "provided",
-                        "render": function (data, type, row) {
-                            if (data) {
-                                return row.acceptanceUserId == null ? 'by service' : row.acceptanceUserEmail;
-                            } else {
-                                return '<button class="acceptbtn" type="submit" onclick="submitAcceptBitcoin(' + row.invoiceId +')">' + acceptButtonLocMessage + '</button>'
-                            }
-                        }
                     }
                 ],
                 "order": []
             });}
     }
+    updateCandleTable();
 
 
 });
