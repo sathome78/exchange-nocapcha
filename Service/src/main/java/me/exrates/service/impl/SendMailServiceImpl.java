c6250757f10a9e1e9027ed97058572aaac640b58
golvazin <wayfarer2006@ukr.net>
2017-03-15 16:33:31 +0200

    fix bitcoin table

--- Service/src/main/java/me/exrates/service/impl/SendMailServiceImpl.java
+++ Service/src/main/java/me/exrates/service/impl/SendMailServiceImpl.java
@@ -34,7 +34,7 @@ public class SendMailServiceImpl implements SendMailService{
 
 	@Override
 	public void sendInfoMail(Email email) {
-	//	sendMail(email, INFO_EMAIL, infoMailSender);
+		sendMail(email, INFO_EMAIL, infoMailSender);
 	}
 
 	private void sendMail(Email email, String fromAddress, JavaMailSender mailSender) {
