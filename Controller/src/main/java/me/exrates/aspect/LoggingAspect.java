a3ce2b37d1ec5b20de2b714476647a637174809a
golvazin <wayfarer2006@ukr.net>
2017-04-03 09:52:07 +0300

    fix

--- Controller/src/main/java/me/exrates/aspect/LoggingAspect.java
+++ Controller/src/main/java/me/exrates/aspect/LoggingAspect.java
@@ -1,11 +1,34 @@
 package me.exrates.aspect;
 
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by OLEG on 31.03.2017.
  */
 @Aspect
+@Log4j2
+@Component
 public class LoggingAspect {
+
+  
+  
+  @AfterThrowing(pointcut = "execution(public * *(..))", throwing = "ex")
+  public void logException(JoinPoint joinPoint, Exception ex) {
+    
+    log.debug(String.format("error in method %s with args: %s", joinPoint.getSignature().getName(),
+            String.join("", Arrays.stream(joinPoint.getArgs()).map(Object::toString).collect(Collectors.toList()))) );
+    
+    log.debug(String.format("exception: %s : %s \n stacktrace: %s", ex.getClass().getSimpleName(), ex.getMessage(), ExceptionUtils.getStackTrace(ex)));
+  
+  }
 
 }
