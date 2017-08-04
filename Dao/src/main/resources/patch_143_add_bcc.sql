3b9c7cc5d77f43e26a3e5162634bcee9bfa8b653
golvazin <wayfarer2006@ukr.net>
2017-08-04 11:42:16 +0300

    fix

--- Dao/src/main/resources/patch_143_add_bcc.sql
+++ Dao/src/main/resources/patch_143_add_bcc.sql
@@ -10,7 +10,7 @@ VALUES ((SELECT id from MERCHANT WHERE name='Bitcoin Cash'),
 
 INSERT INTO `MERCHANT_IMAGE` (`merchant_id`, `image_path`, `image_name`, `currency_id`) VALUES
   ((SELECT id from MERCHANT WHERE name='Bitcoin Cash')
-  , '/client/img/merchants/bitcoin_cash.png', 'Bitcoin Cash', (SELECT id from CURRENCY WHERE name='BCH'));
+    , '/client/img/merchants/bitcoin_cash.png', 'Bitcoin Cash', (SELECT id from CURRENCY WHERE name='BCH'));
 
 INSERT INTO WALLET (user_id, currency_id) select id, (select id from CURRENCY where name='BCH') from USER;
 
@@ -19,6 +19,8 @@ INSERT INTO CURRENCY_LIMIT(currency_id, operation_type_id, user_role_id, min_sum
   FROM CURRENCY_LIMIT WHERE currency_id = (select id from CURRENCY where name = 'BTC');
 
 INSERT INTO `COMPANY_WALLET` (`currency_id`) VALUES ((select id from CURRENCY where name = 'BCH'));
+
+-- !!!!!!!!!!!!!PAIR ORDERS!!!!!!!!!!
 
 INSERT INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden)
 VALUES((select id from CURRENCY where name = 'BCH'), (select id from CURRENCY where name = 'USD'), 'BCH/USD', 10, 0);
@@ -36,5 +38,32 @@ INSERT INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, 
     JOIN USER_ROLE UR
     JOIN ORDER_TYPE OT where CP.name='BCH/BTC';
 
+INSERT INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden)
+VALUES((select id from CURRENCY where name = 'BCH'), (select id from CURRENCY where name = 'EDR'), 'BCH/EDR', 20, 0);
+
+INSERT INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
+  SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
+    JOIN USER_ROLE UR
+    JOIN ORDER_TYPE OT where CP.name='BCH/EDR';
+
+
 INSERT INTO CRYPTO_CORE_WALLET(merchant_id, currency_id, CRYPTO_CORE_WALLET.title_code)
 VALUES ((SELECT id from MERCHANT WHERE name='Bitcoin Cash'), (select id from CURRENCY where name='BCH'), 'bchWallet.title');
+
+INSERT INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, withdraw_block, refill_block, transfer_block)
+VALUES ((SELECT id FROM MERCHANT WHERE name = 'SimpleTransfer'), (select id from CURRENCY where name = 'BCH'), 0.000001, 1, 1, 0);
+
+INSERT INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, withdraw_block, refill_block, transfer_block)
+VALUES ((SELECT id FROM MERCHANT WHERE name = 'VoucherTransfer'), (select id from CURRENCY where name = 'BCH'), 0.000001, 1, 1, 0);
+
+INSERT INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, withdraw_block, refill_block, transfer_block)
+VALUES ((SELECT id FROM MERCHANT WHERE name = 'VoucherFreeTransfer'), (select id from CURRENCY where name = 'BCH'), 0.000001, 1, 1, 0);
+
+INSERT INTO MERCHANT_IMAGE (merchant_id, image_path, image_name, currency_id) VALUES
+  ((SELECT id FROM MERCHANT WHERE name = 'SimpleTransfer'), '/client/img/merchants/transfer.png', 'Transfer', (select id from CURRENCY where name = 'BCH'));
+
+INSERT INTO MERCHANT_IMAGE (merchant_id, image_path, image_name, currency_id) VALUES
+  ((SELECT id FROM MERCHANT WHERE name = 'VoucherTransfer'), '/client/img/merchants/voucher.png', 'Voucher', (select id from CURRENCY where name = 'BCH'));
+
+INSERT INTO MERCHANT_IMAGE (merchant_id, image_path, image_name, currency_id) VALUES
+  ((SELECT id FROM MERCHANT WHERE name = 'VoucherFreeTransfer'), '/client/img/merchants/voucher_free.png', 'Free voucher', (select id from CURRENCY where name = 'BCH'));
