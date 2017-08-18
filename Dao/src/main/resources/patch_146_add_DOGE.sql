9bd6e1dea096e3eea74cfc6fc8731141da0f17ff
golvazin <wayfarer2006@ukr.net>
2017-08-18 10:33:28 +0300

    fix patch

--- Dao/src/main/resources/patch_146_add_DOGE.sql
+++ Dao/src/main/resources/patch_146_add_DOGE.sql
@@ -23,7 +23,7 @@ INSERT INTO `COMPANY_WALLET` (`currency_id`) VALUES ((select id from CURRENCY wh
 -- !!!!!!!!!!!!!PAIR ORDERS!!!!!!!!!!
 
 INSERT INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden)
-VALUES((select id from CURRENCY where name = 'DOGE'), (select id from CURRENCY where name = 'USD'), 'DOGE/USD', 10, 0);
+VALUES((select id from CURRENCY where name = 'DOGE'), (select id from CURRENCY where name = 'USD'), 'DOGE/USD', 281, 0);
 
 INSERT INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
   SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
@@ -31,7 +31,7 @@ INSERT INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, 
     JOIN ORDER_TYPE OT where CP.name='DOGE/USD';
 
 INSERT INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market)
-VALUES((select id from CURRENCY where name = 'DOGE'), (select id from CURRENCY where name = 'BTC'), 'DOGE/BTC', 20, 0, 'BTC');
+VALUES((select id from CURRENCY where name = 'DOGE'), (select id from CURRENCY where name = 'BTC'), 'DOGE/BTC', 282, 0, 'BTC');
 
 INSERT INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
   SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
