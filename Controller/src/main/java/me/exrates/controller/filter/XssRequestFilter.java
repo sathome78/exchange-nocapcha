1cc830842ff6838963a97920e8c02859de8b0136
maks115015 <avto12@i.ua>
2017-09-29 09:48:59 +0300

    fix locale

--- Controller/src/main/java/me/exrates/controller/filter/XssRequestFilter.java
+++ Controller/src/main/java/me/exrates/controller/filter/XssRequestFilter.java
@@ -24,14 +24,14 @@ public class XssRequestFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         try {
-            Map<String, String[]> parameterMap = request.getParameterMap();
+          /*  Map<String, String[]> parameterMap = request.getParameterMap();
             parameterMap.forEach((k,v) -> {
                 Stream.of(v).forEach(va -> {
                     if (!va.equals(Jsoup.clean(va, Whitelist.basic()))) {
                         throw new RuntimeException("xss detected!");
                     }
                 });
-            });
+            });*/
             chain.doFilter(request, response);
         } catch (RuntimeException e) {
             ((HttpServletResponse)response).sendRedirect("/dashboard");
