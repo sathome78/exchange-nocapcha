6c8a5c7b9fcdcb213f683ce6beb2fb28875af45e
ajet21 <ajet5911@gmail.com>
2017-12-12 16:55:53 +0200

    fix

--- Service/src/main/java/me/exrates/service/impl/MoneroServiceImpl.java
+++ Service/src/main/java/me/exrates/service/impl/MoneroServiceImpl.java
@@ -147,7 +147,7 @@ public class MoneroServiceImpl implements MoneroService {
                     public void run() {
                         checkIncomingTransactions();
                     }
-                }, 0, 30, TimeUnit.MINUTES);
+                }, 0, 120, TimeUnit.MINUTES);
             }catch (Exception e){
                 log.error(e);
             }
