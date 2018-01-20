734b2bb03c1432a33f88184c056030d8d11c3d89
golvazin <wayfarer2006@ukr.net>
2018-01-20 16:56:34 +0200

    fix

--- Dao/src/main/resources/patch_176_optimize.sql
+++ Dao/src/main/resources/patch_176_optimize.sql
@@ -1,2 +1,7 @@
-CREATE INDEX currency_pair__cur1_idx ON currency_pair (currency1_id);
-CREATE INDEX currency_pair__cur2_idx ON currency_pair (currency2_id);
+CREATE INDEX currency_pair__cur1_idx ON CURRENCY_PAIR (currency1_id);
+CREATE INDEX currency_pair__cur2_idx ON CURRENCY_PAIR (currency2_id);
+
+DROP INDEX currency_pair ON EXORDERS;
+DROP INDEX status ON EXORDERS;
+
+CREATE INDEX exorders_cp_status_op_type_user_idx ON EXORDERS (currency_pair_id, status_id, operation_type_id, user_id);
