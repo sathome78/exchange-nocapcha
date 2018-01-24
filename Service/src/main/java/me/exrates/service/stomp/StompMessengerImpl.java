2525490b81b9105623709ddce33bf75b39145e84
maks115015 <avto12@i.ua>
2018-01-24 14:52:51 +0200

    add update by intervals

--- Service/src/main/java/me/exrates/service/stomp/StompMessengerImpl.java
+++ Service/src/main/java/me/exrates/service/stomp/StompMessengerImpl.java
@@ -103,9 +103,7 @@ public class StompMessengerImpl implements StompMessenger{
     @Override
     public void sendChartData(final Integer currencyPairId) {
        List<BackDealInterval> intervals = getSubscribedIntervalsForCurrencyPair(currencyPairId);
-
        Map<String, String> data = chartsCache.getData(currencyPairId, intervals);
-
        intervals.forEach(p-> {
             String message = data.get(p.getInterval());
             String destination = "/app/charts/".concat(currencyPairId.toString().concat("/").concat(p.getInterval()));
