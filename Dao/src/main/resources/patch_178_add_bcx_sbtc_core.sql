ad6ba51d98435703aa7f21aa830fef1ae8e4d282
golvazin <wayfarer2006@ukr.net>
2018-01-29 10:39:51 +0200

    fix bcx

--- Dao/src/main/resources/patch_178_add_bcx_sbtc_core.sql
+++ Dao/src/main/resources/patch_178_add_bcx_sbtc_core.sql
@@ -3,3 +3,8 @@ VALUES ((SELECT id from MERCHANT WHERE name='BCX'), (select id from CURRENCY whe
 
 INSERT INTO CRYPTO_CORE_WALLET(merchant_id, currency_id, CRYPTO_CORE_WALLET.title_code)
 VALUES ((SELECT id from MERCHANT WHERE name='SBTC'), (select id from CURRENCY where name='SBTC'), 'sbtcWallet.title');
+
+UPDATE MERCHANT SET service_bean_name = 'bcxServiceImpl' WHERE name = 'BCX';
+
+UPDATE MERCHANT_IMAGE SET image_path = '/client/img/merchants/BCX.png', image_name = 'BCX'
+WHERE merchant_id = (SELECT id from MERCHANT WHERE name='BCX');
