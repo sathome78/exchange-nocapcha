a40f9b1e9ca20fcf6da5102100239ee5ceb91c77
golvazin <wayfarer2006@ukr.net>
2018-02-22 13:34:46 +0200

    change btw logo

--- Dao/src/main/resources/patch_185_add_btw.sql
+++ Dao/src/main/resources/patch_185_add_btw.sql
@@ -35,14 +35,14 @@ INSERT INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, 
     JOIN USER_ROLE UR
     JOIN ORDER_TYPE OT where CP.name='BTW/BTC';
 
-/*INSERT INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
+INSERT INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
 VALUES((select id from CURRENCY where name = 'BTW'), (select id from CURRENCY where name = 'ETH'), 'BTW/ETH', 50, 0, 'FIAT', 'BTW/ETH');
 
 INSERT INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
   SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
     JOIN USER_ROLE UR
     JOIN ORDER_TYPE OT where CP.name='BTW/ETH';
-*/
+
 INSERT INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, withdraw_block, refill_block, transfer_block)
 VALUES ((SELECT id FROM MERCHANT WHERE name = 'SimpleTransfer'), (select id from CURRENCY where name = 'BTW'), 0.000001, 1, 1, 0);
 
@@ -63,9 +63,9 @@ INSERT INTO MERCHANT_IMAGE (merchant_id, image_path, image_name, currency_id) VA
 
 INSERT INTO BOT_LAUNCH_SETTINGS(bot_trader_id, currency_pair_id)
   SELECT BT.id, CP.id FROM BOT_TRADER BT
-    JOIN CURRENCY_PAIR CP WHERE CP.name IN ('BTW/USD', 'BTW/BTC'/*, 'BTW/ETH'*/);
+    JOIN CURRENCY_PAIR CP WHERE CP.name IN ('BTW/USD', 'BTW/BTC', 'BTW/ETH');
 
 INSERT INTO BOT_TRADING_SETTINGS(bot_launch_settings_id, order_type_id)
   SELECT BLCH.id, OT.id FROM BOT_LAUNCH_SETTINGS BLCH
     JOIN ORDER_TYPE OT
-  WHERE BLCH.currency_pair_id IN (SELECT id FROM CURRENCY_PAIR WHERE name IN ('BTW/USD', 'BTW/BTC'/*, 'BTW/ETH'*/));
+  WHERE BLCH.currency_pair_id IN (SELECT id FROM CURRENCY_PAIR WHERE name IN ('BTW/USD', 'BTW/BTC', 'BTW/ETH'));
