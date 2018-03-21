d531987103b4827544080630d9d09a90536545b5
maks115015 <avto12@i.ua>
2018-03-21 14:53:31 +0200

    fix

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -652,7 +652,7 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         return new EthTokenServiceImpl(
                 tokensList,
                 "NAC",
-                "NAC", true, Convert.Unit.ETHER);
+                "NAC", true, ExConvert.Unit.ETHER);
     }
 
 //    Qtum tokens:
