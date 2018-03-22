b67c90c7af0ea1c37000dd0e06819f2c0c04de21
maks115015 <avto12@i.ua>
2018-03-22 12:26:16 +0200

    add

--- Controller/src/main/java/me/exrates/controller/PublicController.java
+++ Controller/src/main/java/me/exrates/controller/PublicController.java
@@ -1,11 +1,13 @@
 package me.exrates.controller;
 
+import lombok.extern.log4j.Log4j2;
 import me.exrates.model.dto.CoinmarketApiDto;
 import me.exrates.model.dto.CoinmarketApiJsonDto;
 import me.exrates.model.dto.mobileApiDto.MerchantCurrencyApiDto;
 import me.exrates.model.vo.BackDealInterval;
 import me.exrates.service.MerchantService;
 import me.exrates.service.OrderService;
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,27 +17,45 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
 /**
  * Created by Valk on 11.05.2016.
  */
-
 @RestController
 public class PublicController {
     private static final Logger LOGGER = LogManager.getLogger(PublicController.class);
 
     @Autowired
+    private
     OrderService orderService;
+
+    private final Object synchronizer = new Object();
+
+    private ConcurrentMap<String, CoinmarketApiJsonDto> cachedData = new ConcurrentHashMap<>();
+
+    private ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+
+    @PostConstruct
+    public void init() {
+        scheduler.scheduleAtFixedRate(()-> {
+            synchronized(synchronizer) {
+                Map<String, CoinmarketApiJsonDto> newData = getData(null);
+                cachedData = new ConcurrentHashMap<>(newData);
+            }
+        }, 0, 30, TimeUnit.MINUTES);
+    }
 
     @RequestMapping(value = "/public/coinmarketcap/ticker", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public Map<String, CoinmarketApiJsonDto> tiker(@RequestParam(required = false) String currencyPair, HttpServletRequest request) {
         long before = System.currentTimeMillis();
-        List<CoinmarketApiDto> list = null;
         String ip = "";
         try {
             if (currencyPair != null) {
@@ -45,8 +65,14 @@ public class PublicController {
             if (ip == null) {
                 ip = request.getRemoteHost();
             }
-            list = orderService.getCoinmarketDataForActivePairs(currencyPair, new BackDealInterval("24 HOUR"));
-            Map<String, CoinmarketApiJsonDto> result = list.stream().collect(Collectors.toMap(dto -> dto.getCurrency_pair_name().replace('/','_'), CoinmarketApiJsonDto::new));
+            Map<String, CoinmarketApiJsonDto> result;
+            if (StringUtils.isEmpty(currencyPair) && cachedData != null && !cachedData.isEmpty()) {
+                synchronized(synchronizer) {
+                    result = cachedData;
+                }
+            } else {
+                result = getData(currencyPair);
+            }
             long after = System.currentTimeMillis();
             LOGGER.debug(String.format("completed... from ip: %s ms: %s", ip, (after - before)));
             return result;
@@ -56,4 +82,11 @@ public class PublicController {
             throw e;
         }
     }
+
+    private Map<String, CoinmarketApiJsonDto> getData(String currencyPair) {
+        List<CoinmarketApiDto> list = orderService.getCoinmarketDataForActivePairs(currencyPair, new BackDealInterval("24 HOUR"));
+        return list.stream().collect(Collectors.toMap(dto -> dto.getCurrency_pair_name().replace('/', '_'), CoinmarketApiJsonDto::new));
+    }
+
+
 }
