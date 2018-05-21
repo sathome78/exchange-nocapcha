9eca5e409d8bdb75bab085be891aa6b0b6aad75f
ajet21 <ajet5911@gmail.com>
2018-05-21 17:09:08 +0300

    fix

--- Dao/src/main/java/me/exrates/dao/impl/WalletDaoImpl.java
+++ Dao/src/main/java/me/exrates/dao/impl/WalletDaoImpl.java
@@ -1199,7 +1199,7 @@ public class WalletDaoImpl implements WalletDao {
   @Override
   public List<ExternalWalletsDto> getBalancesWithExternalWallets() {
     String sql = "SELECT CUR.name AS currency_name, CUR.id as currency_id, AGR.total_balance, main_wallet_balance, reserve_wallet_balance, cold_wallet_balance, IFNULL(MC.merchant_id, 0) as merchant_id \n" +
-            "FROM (   SELECT STRAIGHT_JOIN W.currency_id, URGF.name AS feature_name, SUM(IFNULL(W.active_balance, 0)) + SUM(IFNULL(W.reserved_balance, 0)) AS total_balance   \n" +
+            "FROM (   SELECT STRAIGHT_JOIN W.currency_id, SUM(IFNULL(W.active_balance, 0)) + SUM(IFNULL(W.reserved_balance, 0)) AS total_balance   \n" +
             "FROM WALLET W     JOIN USER U ON U.id = W.user_id     JOIN USER_ROLE UR ON U.roleid = UR.id     \n" +
             "JOIN USER_ROLE_REPORT_GROUP_FEATURE URGF ON UR.user_role_report_group_feature_id = URGF.id AND UR.user_role_report_group_feature_id IN (1,2)   GROUP BY W.currency_id   ) AGR   \n" +
             "JOIN CURRENCY CUR ON AGR.currency_id = CUR.id AND CUR.hidden = 0 \n" +
