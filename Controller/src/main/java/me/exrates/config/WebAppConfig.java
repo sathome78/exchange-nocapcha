e5687a077f4d87712eaf1b6a78a097999e6c6371
Artyom Sargsyan <sarksiyana@gmail.com>
2018-07-10 12:36:30 +0400

    EX-429: add CHE token.

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -1006,6 +1006,16 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
     }
 
 
+    @Bean(name = "cheServiceImpl")
+    public EthTokenService cheService() {
+        List<String> tokensList = new ArrayList<>();
+        tokensList.add("0x632f62fcf63cb56380ffd27d63afcf5f1349f73f");
+        return new EthTokenServiceImpl(
+                tokensList,
+                "CHE",
+                "CHE", false, ExConvert.Unit.AIWEI);
+    }
+
     @Bean(name = "engtServiceImpl")
     public EthTokenService engtService() {
         List<String> tokensList = new ArrayList<>();
