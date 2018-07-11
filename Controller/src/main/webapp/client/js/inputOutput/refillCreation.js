840eb3d4cb7ff52e59fe13042ad2a303a6e26199
dziubak <vlad.dziubak@gmail.com>
2018-07-11 23:43:11 +0300

    EX-307 | Add modal view | Add locale

--- Controller/src/main/webapp/client/js/inputOutput/refillCreation.js
+++ Controller/src/main/webapp/client/js/inputOutput/refillCreation.js
@@ -142,7 +142,11 @@ $(function refillCreation() {
             $refillParamsDialog.find('#message').html(message ? message : '');
             $refillParamsDialog.find('#payment-qr').html('');
             $refillParamsDialog.find("#continue-btn").off('click').on('click', function () {
-                successNoty("Refill request is valid for 48 hours");
+
+            $("#warning-remporary-validity-refill-request-merchant").modal({
+                backdrop: 'static'
+            });
+
                 window.open("about:blank","newwin");
                 if (!checkRefillParamsEnter()) {
                     return;
