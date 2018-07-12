sathome78/exchange

EX-515: add DACC token.

Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
@@ -10,10 +10,10 @@ public class TokenWrappersGenerator {
     public static void main(String[] args) throws Exception {
         SolidityFunctionWrapperGenerator.run(new String[]{
                 "generate",
-                "/home/artyom/eth/ADB.bin",
-                "/home/artyom/eth/ADB.abi",
+                "/home/sevak/eth/DACC.bin",
+                "/home/sevak/eth/DACC.abi",
                 "-o",
-                "/home/artyom/IHG/leviygordz/exrates/Service/src/main/java",
+                "/home/sevak/IdeaProjects/exrates/Service/src/main/java",
                 "-p",
                 "me.exrates.service.ethereum.ethTokensWrappers"});
     }

sathome78/exchange-nocapcha

Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
@@ -10,10 +10,10 @@ public class TokenWrappersGenerator {
     public static void main(String[] args) throws Exception {
         SolidityFunctionWrapperGenerator.run(new String[]{
                 "generate",
-                "/home/artyom/eth/ADB.bin",
-                "/home/artyom/eth/ADB.abi",
+                "/home/artyom/eth/DACC.bin",
+                "/home/artyom/eth/DACC.abi",
                 "-o",
-                "/home/artyom/IHG/leviygordz/exrates/Service/src/main/java",
+                "/home/artyom/IdeaProjects/exrates/Service/src/main/java",
                 "-p",
                 "me.exrates.service.ethereum.ethTokensWrappers"});
     }