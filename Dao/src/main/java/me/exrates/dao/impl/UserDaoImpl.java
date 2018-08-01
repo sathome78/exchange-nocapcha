656816f7fe51423de47dfd745de47015bff13048
dziubak <vlad.dziubak@gmail.com>
2018-08-01 16:17:57 +0300

    EX-703 | Add sql patch | When user create - add access to user operations
    (cherry picked from commit c3371d4)

--- Dao/src/main/java/me/exrates/dao/impl/UserDaoImpl.java
+++ Dao/src/main/java/me/exrates/dao/impl/UserDaoImpl.java
@@ -147,6 +147,8 @@ public class UserDaoImpl implements UserDao {
     String sqlWallet = "INSERT INTO WALLET (currency_id, user_id) select id, :user_id from CURRENCY;";
     String sqlNotificationOptions = "INSERT INTO NOTIFICATION_OPTIONS(notification_event_id, user_id, send_notification, send_email) " +
         "select id, :user_id, default_send_notification, default_send_email FROM NOTIFICATION_EVENT; ";
+    String sqlUserOperationAuthority = "INSERT INTO USER_OPERATION_AUTHORITY (user_id, user_operation_id) " +
+            "SELECT :user_id, id FROM USER_OPERATION;";
     Map<String, String> namedParameters = new HashMap<String, String>();
     namedParameters.put("email", user.getEmail());
     namedParameters.put("nickname", user.getNickname());
@@ -170,7 +172,8 @@ public class UserDaoImpl implements UserDao {
     Map<String, Integer> userIdParamMap = Collections.singletonMap("user_id", userId);
 
     return namedParameterJdbcTemplate.update(sqlWallet, userIdParamMap) > 0
-        && namedParameterJdbcTemplate.update(sqlNotificationOptions, userIdParamMap) > 0;
+        && namedParameterJdbcTemplate.update(sqlNotificationOptions, userIdParamMap) > 0
+            && namedParameterJdbcTemplate.update(sqlUserOperationAuthority, userIdParamMap) > 0;
   }
 
   @Override
