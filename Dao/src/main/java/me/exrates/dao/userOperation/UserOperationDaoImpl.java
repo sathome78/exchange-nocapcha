e316cf629350308cf54377756a7bf2f880614efa
dziubak <vlad.dziubak@gmail.com>
2018-08-01 12:19:11 +0300

    EX-703 | Add input/ouput/transfer block
    (cherry picked from commit 6df7c55)

--- Dao/src/main/java/me/exrates/dao/userOperation/UserOperationDaoImpl.java
+++ Dao/src/main/java/me/exrates/dao/userOperation/UserOperationDaoImpl.java
@@ -1,33 +1,14 @@
 package me.exrates.dao.userOperation;
 
 import lombok.extern.log4j.Log4j;
-import me.exrates.dao.MerchantDao;
-import me.exrates.model.Merchant;
-import me.exrates.model.MerchantCurrency;
-import me.exrates.model.MerchantImage;
-import me.exrates.model.dto.*;
-import me.exrates.model.dto.merchants.btc.CoreWalletDto;
-import me.exrates.model.dto.mobileApiDto.MerchantCurrencyApiDto;
-import me.exrates.model.dto.mobileApiDto.MerchantImageShortenedDto;
-import me.exrates.model.dto.mobileApiDto.TransferMerchantApiDto;
-import me.exrates.model.enums.OperationType;
-import me.exrates.model.enums.UserRole;
-import me.exrates.model.userOperation.UserOperationAuthorityEntity;
 import me.exrates.model.userOperation.UserOperationAuthorityOption;
 import me.exrates.model.userOperation.enums.UserOperationAuthority;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -44,19 +25,18 @@ public class UserOperationDaoImpl implements UserOperationDao {
 
   @Override
   public boolean getStatusAuthorityForUserByOperation(int userId, UserOperationAuthority userOperationAuthority) {
-    final String sql = "SELECT user_id, user_operation_id, enabled FROM USER_OPERATION_AUTHORITY " +
+    final String sql = "SELECT user_operation_id FROM USER_OPERATION_AUTHORITY " +
             "WHERE user_id=:userId AND user_operation_id=:userOperationId AND enabled=1";
 
-    Map<String, Integer> namedParameters = new HashMap<>();
-    namedParameters.put("userId", userId);
-    namedParameters.put("userOperationId", userOperationAuthority.getOperationId());
+    MapSqlParameterSource params = new MapSqlParameterSource();
+    params.addValue("userId", userId);
+    params.addValue("userOperationId", userOperationAuthority.getOperationId());
 
-    return namedParameterJdbcTemplate.query(sql, namedParameters, (rs, i) -> {
-      UserOperationAuthorityEntity userOperationAuthorityEntity = new UserOperationAuthorityEntity();
-      userOperationAuthorityEntity.setUserId(rs.getInt("user_id"));
-      userOperationAuthorityEntity.setUserOperationId(rs.getInt("user_operation_id"));
-      return userOperationAuthorityEntity;
-    });
+    try {
+      return namedParameterJdbcTemplate.queryForObject(sql, params, Integer.class) > 0;
+    } catch (EmptyResultDataAccessException e) {
+    return false;
+    }
   }
 
   @Override
