d7a594cebd0c34f7232eb231308d88e62fa7bb77
dziubak <vlad.dziubak@gmail.com>
2018-08-27 21:28:44 +0300

    EX-878 | Ethereum token - TUSD

--- Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
+++ Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
@@ -10,10 +10,10 @@ public class TokenWrappersGenerator {
     public static void main(String[] args) throws Exception {
         SolidityFunctionWrapperGenerator.run(new String[]{
                 "generate",
-                "/home/yagi/crypto/eth/KWATT.bin",
-                "/home/yagi/crypto/eth/KWATT.abi",
+                "/home/yagi/crypto/eth/TUSD.bin",
+                "/home/yagi/crypto/eth/TUSD.abi",
                 "-o",
-                "/Users/exrates/Vlad/exrates/Service/src/main/java",
+                "/home/yagi/workspace/becomeJavaSenior/exrates/Service/src/main/java",
                 "-p",
                 "me.exrates.service.ethereum.ethTokensWrappers"});
     }
