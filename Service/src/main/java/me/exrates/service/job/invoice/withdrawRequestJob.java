6a72f5bbd1f280559aa4493551aabc380970750a
maks11110 <avto12@i.ua>
2018-09-11 18:19:17 +0300

    fix

--- Service/src/main/java/me/exrates/service/job/invoice/withdrawRequestJob.java
+++ Service/src/main/java/me/exrates/service/job/invoice/withdrawRequestJob.java
@@ -63,7 +63,7 @@ public class withdrawRequestJob {
   @PostConstruct
   private void initSchedule() {
     scheduler.scheduleAtFixedRate(this::setInPostingStatus, 3, 1, TimeUnit.MINUTES);
-    scheduler.scheduleAtFixedRate(this::postWithdraw, 3, 13, TimeUnit.MINUTES);
+    scheduler.scheduleAtFixedRate(this::postWithdraw, 3, 1, TimeUnit.MINUTES);
   }
 
  // @Scheduled(initialDelay = 1000, fixedDelay = 1000 * 60 * 1)
