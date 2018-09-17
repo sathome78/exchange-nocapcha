ab5fac0d56407bb5132dc267f44cbefec5c5bfbd
dziubak <vlad.dziubak@gmail.com>
2018-09-17 10:59:20 +0300

    Ethereum token - NER | Fix

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -1320,7 +1320,7 @@ public class WebAppConfig extends WebMvcConfigurerAdapter{
     @Bean(name = "nerServiceImpl")
     public EthTokenService nerService() {
         List<String> tokensList = new ArrayList<>();
-        tokensList.add("0x7a07e1a0c2514d51132183ecfea2a880ec3b7648");
+        tokensList.add("0xee5dfb5ddd54ea2fb93b796a8a1b83c3fe38e0e6");
         return new EthTokenServiceImpl(
                 tokensList,
                 "NER",
