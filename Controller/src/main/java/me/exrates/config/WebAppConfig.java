0ac12a39095f3341337b188f3261891713e86458
dziubak <vlad.dziubak@gmail.com>
2018-10-01 13:39:14 +0300

    Ethereum token - WTL || Main part

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -1439,6 +1439,16 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
                 "BNC", true, ExConvert.Unit.AIWEI);
     }
 
+    @Bean(name = "wltServiceImpl")
+    public EthTokenService wltService() {
+        List<String> tokensList = new ArrayList<>();
+        tokensList.add("0x9a0587eae7ef64b2b38a10442a44cfa43edd7d2a");
+        return new EthTokenServiceImpl(
+                tokensList,
+                "WLT",
+                "WLT", true, ExConvert.Unit.ETHER);
+    }
+
     //    Qtum tokens:
     @Bean(name = "spcServiceImpl")
     public QtumTokenService spcService() {
