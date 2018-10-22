99d7565c334d6154a72a38a34060c041323651e4
Vlad Dziubak <38761116+dziubak@users.noreply.github.com>
2018-10-22 16:56:16 +0300

    Delete V1.4__API_request_limits.sql

--- Controller/src/main/resources/db/migration/V1.4__API_request_limits.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS USER_API;
-
-CREATE TABLE USER_API
-(
-  id        INT   UNSIGNED  PRIMARY KEY   NOT NULL AUTO_INCREMENT,
-  user_id   INT                           NOT NULL,
-  attempts  INT                           NOT NULL,
-  CONSTRAINT user_api_user_id_fk FOREIGN KEY (user_id) REFERENCES USER (id)
-);
