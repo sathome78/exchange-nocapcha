b5728ff258385c3c8e2e83a389ee7bd85dd3bd4a
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2018-11-05 17:12:54 +0200

    add migration: change fields type range

--- Controller/src/main/resources/db/migration/V1.33__recreate_internal_wallets_table.sql
+++ Controller/src/main/resources/db/migration/V1.33__recreate_internal_wallets_table.sql
@@ -5,12 +5,12 @@ CREATE TABLE INTERNAL_WALLET_BALANCES
   id                INT(40) UNSIGNED PRIMARY KEY   NOT NULL AUTO_INCREMENT,
   currency_id       INT(40)                        NOT NULL,
   role_id           INT(40)                        NOT NULL,
-  usd_rate          NUMERIC(19, 8)                          DEFAULT 0,
-  btc_rate          NUMERIC(19, 8)                          DEFAULT 0,
-  total_balance     NUMERIC(30, 8)                          DEFAULT 0,
-  total_balance_usd NUMERIC(30, 8)                          DEFAULT 0,
-  total_balance_btc NUMERIC(30, 8)                          DEFAULT 0,
-  last_updated_at   TIMESTAMP                      NULL     DEFAULT CURRENT_TIMESTAMP,
+  usd_rate          NUMERIC(19, 12)                           DEFAULT 0,
+  btc_rate          NUMERIC(19, 12)                           DEFAULT 0,
+  total_balance     NUMERIC(30, 8)                            DEFAULT 0,
+  total_balance_usd NUMERIC(30, 8)                            DEFAULT 0,
+  total_balance_btc NUMERIC(30, 8)                            DEFAULT 0,
+  last_updated_at   TIMESTAMP                      NULL       DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT internal_wallet_balances_currency_id_fk FOREIGN KEY (currency_id) REFERENCES CURRENCY (id),
   CONSTRAINT internal_wallet_balances_role_id_fk FOREIGN KEY (role_id) REFERENCES USER_ROLE (id)
 );
