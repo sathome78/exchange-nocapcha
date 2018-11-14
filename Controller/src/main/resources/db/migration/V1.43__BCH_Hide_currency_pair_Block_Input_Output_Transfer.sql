78313b533ff2150e4172273d8e7b8d4409993899
Vlad Dziubak <38761116+dziubak@users.noreply.github.com>
2018-11-15 01:22:46 +0200

    Update V1.43__BCH_Hide_currency_pair_Block_Input_Output_Transfer.sql

--- Controller/src/main/resources/db/migration/V1.43__BCH_Hide_currency_pair_Block_Input_Output_Transfer.sql
+++ Controller/src/main/resources/db/migration/V1.43__BCH_Hide_currency_pair_Block_Input_Output_Transfer.sql
@@ -1,6 +1,6 @@
 UPDATE CURRENCY_PAIR SET hidden = 1
-WHERE currency1_id = (SELECT id FROM CURRENCY WHERE name="BCH")
-OR currency2_id = (SELECT id FROM CURRENCY WHERE name="BCH");
+WHERE currency1_id = (SELECT id FROM CURRENCY WHERE name='BCH')
+OR currency2_id = (SELECT id FROM CURRENCY WHERE name='BCH');
 
 UPDATE MERCHANT_CURRENCY SET withdraw_block = 1, refill_block = 1, transfer_block = 1
-WHERE currency_id = (SELECT id FROM CURRENCY WHERE name="BCH");
+WHERE currency_id = (SELECT id FROM CURRENCY WHERE name='BCH');
