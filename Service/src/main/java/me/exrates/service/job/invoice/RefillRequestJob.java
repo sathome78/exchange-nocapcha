682439dd0a0eb514ac6308a16881fee9c0d747cd
NMaslukov <JavaNoob14679987>
2018-11-15 14:31:49 +0200

    fix

--- Service/src/main/java/me/exrates/service/job/invoice/RefillRequestJob.java
+++ Service/src/main/java/me/exrates/service/job/invoice/RefillRequestJob.java
@@ -45,8 +45,7 @@ public class RefillRequestJob {
    */
   @Scheduled(initialDelay = 180000, fixedDelay = 1000 * 60 * 5)
   public void refillCheckPaymentsForCoins() {
-      String[] merchantNames = new String[]{"QRK", "LBTC", "LPC", "XFC", "DDX", "ABBC", "CBC"};
-
+      String[] merchantNames = new String[]{"QRK", "LBTC", "LPC", "XFC", "DDX", "MBC", "BTCP", "CLX", "ABBC", "CBC", "BTCZ"};
       for (String coin : merchantNames) {
           getBitcoinServiceByMerchantName(coin).scanForUnprocessedTransactions(null);
       }
