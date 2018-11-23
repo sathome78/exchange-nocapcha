9e585d14bf5f0ad2080135b325aa29fee01b34ca
dziubak <vlad.dziubak@gmail.com>
2018-11-23 12:34:39 +0200

    C2-27 | Change sql patch

--- Controller/src/main/resources/db/migration/V1.49__Ethereum_token_MODL.sql
+++ Controller/src/main/resources/db/migration/V1.49__Ethereum_token_MODL.sql
@@ -71,3 +71,14 @@ INSERT IGNORE INTO BOT_TRADING_SETTINGS(bot_launch_settings_id, order_type_id)
   SELECT BLCH.id, OT.id FROM BOT_LAUNCH_SETTINGS BLCH
     JOIN ORDER_TYPE OT
   WHERE BLCH.currency_pair_id IN (SELECT id FROM CURRENCY_PAIR WHERE name IN ('MODL/USD', 'MODL/BTC', 'MODL/ETH'));
+
+INSERT IGNORE INTO INTERNAL_WALLET_BALANCES (currency_id, role_id)
+  SELECT cur.id AS currency_id, ur.id AS role_id
+  FROM CURRENCY cur CROSS JOIN USER_ROLE ur
+  WHERE cur.name IN ('MODL')
+  ORDER BY cur.id, ur.id;
+
+INSERT IGNORE INTO COMPANY_EXTERNAL_WALLET_BALANCES (currency_id)
+  SELECT cur.id
+  FROM CURRENCY cur
+  WHERE cur.name IN ('MODL');
