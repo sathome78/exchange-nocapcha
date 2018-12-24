dc68e4ede22ce6e3bfdf57aad948d8d6d872016d
NMaslukov <JavaNoob14679987>
2018-12-24 13:22:19 +0200

    95% done

---
+++ Controller/src/main/resources/db/migration/V1.66__ADD_KO_CHAT.sql
@@ -0,0 +1,14 @@
+create table CHAT_KO
+(
+  id           int          not null
+    primary key,
+  user_id      int          not null,
+  body         varchar(256) not null,
+  message_time datetime     not null,
+  constraint CHAT_KO_ibfk_1
+    foreign key (user_id) references USER (id)
+      on update cascade
+);
+
+create index CHAT_RU
+  on CHAT_KO (user_id);
