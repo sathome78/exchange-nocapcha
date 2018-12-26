1886d440fbd9fbde3a4cd06a77a6a47ad1138b6d
NMaslukov <JavaNoob14679987>
2018-12-26 14:51:19 +0200

    done

--- Controller/src/main/java/me/exrates/controller/ChatKoController.java
+++ Controller/src/main/java/me/exrates/controller/ChatKoController.java
@@ -5,11 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class ChatKoController {
 
     @GetMapping("/chat-ko/iframe.html")
-    public RedirectView redirect(){
+    public RedirectView redirect(HttpServletResponse response){
+        response.addCookie(new Cookie("myAppLocaleCookie", "ko"));
         return new RedirectView("/dashboard");
     }
 }
