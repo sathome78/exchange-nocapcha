2a4eff485724dea1afd48a00d17f6d6a6e91ede4
NMaslukov <JavaNoob14679987>
2018-12-28 11:28:07 +0200

    KOREAN FIX ABOUT US

---
+++ Controller/src/main/resources/db/migration/V1.72__KOREAN_ADD_ABOUT_US.sql
@@ -0,0 +1,45 @@
+INSERT IGNORE INTO birzha.NEWS_VARIANTS_EXT (news_id, title, language, brief, content, added_date, active, visit_count, tags, sync_to_wallet_date, updated_date) VALUES (1, null, 'ko', null, '<p><strong>Exrates</strong> is an actively-growing platform. Creating comfortable and practical conditions for using the Exchange is our priority.</p>
+<p><strong>This is</strong></p>
+<ul>
+<li><strong><u>Minimum commission:</u></strong></li>
+</ul>
+<p>The Exchange takes a minimum commission for input and output means;</p>
+<ul>
+<li><strong><u>Maximum security:</u></strong></li>
+</ul>
+<p>Potential errors, that could harm the data storage security, have been provided and corrected.</p>
+<ul>
+<li><strong><u>Easy usability:</u></strong></li>
+</ul>
+<p>We have engaged experienced users of exchange systems and assistants without experience with exchanges for help in the development of usability.</p>
+<p>The results is a high-quality optimal concept that will change and improve according to the new trends in the development of exchanges.</p>
+<ul>
+<li><strong><u>Сurrency</u></strong></li>
+</ul>
+<p>Exchange provides an opportunity not only to exchange currency such heavyweights as Bitcoin, but the actual perspective cryptocurrency such as EDRCoin.</p>
+<p>You can also refill/withdraw and exchange USD, EUR, RUB, UAH, IDR to cryptocurrency.</p>
+<ul>
+<li><strong><u>Transfers between users</u></strong></li>
+</ul>
+<p>You need to know ONLY users nickname at the system for money transfer.</p>
+<ul>
+<li><strong><u>ONLINE LIVE SUPPORT CHAT 24/7</u></strong></li>
+</ul>
+<p>Our support team is always online and ready to help you!</p>
+<ul>
+<li><strong><u>Not only English localization</u></strong></li>
+</ul>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; You can choose the most comfortable language</p>
+<p>English</p>
+<p>Русский</p>
+<p>中国</p>
+<p>Indonesia</p>
+<p>العربية</p>
+<p><strong>Exchange tools:</strong></p>
+<p>Our playground is suitable for both beginners who want to exchange their money with another user at the market rate, and experienced traders.</p>
+<p>&nbsp;</p>
+<p><strong><u>We are</u></strong><strong><u> work</u></strong><strong><u>ing</u></strong><strong><u> hard for making Exrates.me better!</u></strong></p>
+<p><strong><u>We improve our service and looking for new best options for growing up!</u></strong></p>
+<p><strong><u>We want YOU to feel comfortable and satisfied! </u></strong></p>
+<p><strong>We want YOU to choose US!</strong></p>
+<p>&nbsp;</p>', '2017-02-14 18:00:48', true, 0, null, null, '2017-02-14 18:02:56');
