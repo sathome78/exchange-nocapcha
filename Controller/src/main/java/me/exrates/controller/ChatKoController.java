0c982cccf7de81524f014f9d73853c5b60628e98
NMaslukov <JavaNoob14679987>
2018-12-28 14:08:30 +0200

    KOREAN fix controller

--- Controller/src/main/java/me/exrates/controller/ChatKoController.java
+++ Controller/src/main/java/me/exrates/controller/ChatKoController.java
@@ -2,18 +2,25 @@ package me.exrates.controller;
 
 import org.glassfish.jersey.spi.Contract;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.support.RequestContextUtils;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ChatKoController {
 
-//    @GetMapping("/chat-ko/iframe.html")
-//    public RedirectView redirect(HttpServletResponse response){
-//        response.addCookie(new Cookie("myAppLocaleCookie", "ko"));
-//        return new RedirectView("/dashboard");
-//    }
+    @GetMapping("/chat-ko/iframe.html")
+    public RedirectView redirect(HttpServletRequest request, HttpServletResponse response){
+
+        LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+        localeResolver.setLocale(request, response, StringUtils.parseLocaleString("ko"));
+        return new RedirectView("/dashboard");
+    }
 }
