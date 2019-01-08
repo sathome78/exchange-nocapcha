9c15e3d6667b214db37f3f68778ccc66c1c12527
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-01-08 17:04:00 +0200

    DEVEX-TASK-FOR-CANCEL-ORDER (add exception handler)

--- Service/src/main/java/me/exrates/service/exception/OrderCancellingException.java
+++ Service/src/main/java/me/exrates/service/exception/OrderCancellingException.java
@@ -1,10 +1,16 @@
 package me.exrates.service.exception;
 
-/**
- * Created by Valk on 23.05.2016.
- */
-public class OrderCancellingException extends RuntimeException{
+public class OrderCancellingException extends RuntimeException {
+
     public OrderCancellingException(String message) {
         super(message);
+    }
+
+    public OrderCancellingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OrderCancellingException(Throwable cause) {
+        super(cause);
     }
 }
