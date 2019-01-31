91c86a389f6022643cbbd7c4bb2a4fc1b2598231
maks11110 <avto12@i.ua>
2019-01-31 14:36:17 +0200

    fixes

--- Controller/src/main/resources/db/migration/V1.108__add_blocked_field.sql
+++ Controller/src/main/resources/db/migration/V1.108__add_blocked_field.sql
@@ -1,13 +1,1 @@
 ALTER TABLE REFILL_REQUEST_ADDRESS ADD blocked tinyint(1) not null default false;
-
-ALTER TABLE
-`CURRENCY_PAIR`
-  MODIFY COLUMN
-  `market` enum(
-  'USD',
-  'BTC',
-  'FIAT',
-  'ETH',
-  'ICO',
-  'USDT'
-);
