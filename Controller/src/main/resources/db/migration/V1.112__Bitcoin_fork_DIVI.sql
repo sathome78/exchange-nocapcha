c0a4d5b14da633d956c97b198202cf1fa9ed6ec5
Hryhorii Taranyk <grigoriytaranik@gmail.com>
2019-02-06 16:29:29 +0200

    change BTC pair only

--- Controller/src/main/resources/db/migration/V1.112__Bitcoin_fork_DIVI.sql
+++ Controller/src/main/resources/db/migration/V1.112__Bitcoin_fork_DIVI.sql
@@ -22,14 +22,6 @@ INSERT IGNORE INTO CURRENCY_LIMIT(currency_id, operation_type_id, user_role_id, 
 
 INSERT IGNORE INTO `COMPANY_WALLET` (`currency_id`) VALUES ((select id from CURRENCY where name = 'DIVI'));
 
-INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, ticker_name)
-VALUES((select id from CURRENCY where name = 'DIVI'), (select id from CURRENCY where name = 'USD'), 'DIVI/USD', 170, 0, 'DIVI/USD');
-
-INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
-  SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
-  JOIN USER_ROLE UR
-  JOIN ORDER_TYPE OT where CP.name='DIVI/USD';
-
 INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
 VALUES((select id from CURRENCY where name = 'DIVI'), (select id from CURRENCY where name = 'BTC'), 'DIVI/BTC', 160, 0, 'BTC', 'DIVI/BTC');
 
@@ -37,14 +29,6 @@ INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_ty
   SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
     JOIN USER_ROLE UR
     JOIN ORDER_TYPE OT where CP.name='DIVI/BTC';
-
-INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
-VALUES((select id from CURRENCY where name = 'DIVI'), (select id from CURRENCY where name = 'ETH'), 'DIVI/ETH', 160, 0, 'ETH', 'DIVI/ETH');
-
-INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
-  SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
-    JOIN USER_ROLE UR
-    JOIN ORDER_TYPE OT where CP.name='DIVI/ETH';
 
 INSERT IGNORE INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, withdraw_block, refill_block, transfer_block)
 VALUES ((SELECT id FROM MERCHANT WHERE name = 'SimpleTransfer'), (select id from CURRENCY where name = 'DIVI'), 0.0001, 1, 1, 0);
@@ -66,12 +50,12 @@ INSERT IGNORE INTO MERCHANT_IMAGE (merchant_id, image_path, image_name, currency
 
 INSERT IGNORE INTO BOT_LAUNCH_SETTINGS(bot_trader_id, currency_pair_id)
   SELECT BT.id, CP.id FROM BOT_TRADER BT
-    JOIN CURRENCY_PAIR CP WHERE CP.name IN ('DIVI/USD', 'DIVI/BTC', 'DIVI/ETH');
+    JOIN CURRENCY_PAIR CP WHERE CP.name IN ('DIVI/BTC');
 
 INSERT IGNORE INTO BOT_TRADING_SETTINGS(bot_launch_settings_id, order_type_id)
   SELECT BLCH.id, OT.id FROM BOT_LAUNCH_SETTINGS BLCH
     JOIN ORDER_TYPE OT
-  WHERE BLCH.currency_pair_id IN (SELECT id FROM CURRENCY_PAIR WHERE name IN ('DIVI/USD', 'DIVI/BTC', 'DIVI/ETH'));
+  WHERE BLCH.currency_pair_id IN (SELECT id FROM CURRENCY_PAIR WHERE name IN ('DIVI/BTC'));
 
 INSERT IGNORE INTO CRYPTO_CORE_WALLET(merchant_id, currency_id, CRYPTO_CORE_WALLET.title_code, passphrase)
 VALUES ((SELECT id from MERCHANT WHERE name='DIVI'), (select id from CURRENCY where name='DIVI'), 'CRYPWallet.title', 'pass123');
