c73ae36d4d5b520981376a8d3dc1a8d923f45402
maks11110 <avto12@i.ua>
2019-02-20 15:31:52 +0200

    add tests

---
+++ Controller/src/test/java/me/exrates/controller/openAPI/config/WebAppTestConfig.java
@@ -0,0 +1,57 @@
+package me.exrates.controller.openAPI.config;
+
+import me.exrates.security.service.OpenApiAuthService;
+import me.exrates.service.CurrencyService;
+import me.exrates.service.OrderService;
+import me.exrates.service.UserService;
+import me.exrates.service.WalletService;
+import me.exrates.service.userOperation.UserOperationService;
+import org.mockito.Mockito;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+        "me.exrates.controller.openAPI",
+})
+public class WebAppTestConfig {
+
+    @Bean
+    public OrderService orderService() {
+        return Mockito.mock(OrderService.class);
+    }
+
+    @Bean
+    public UserService userService(){
+        return Mockito.mock(UserService.class);
+    }
+
+    @Bean
+    public CurrencyService currencyService(){
+        return Mockito.mock(CurrencyService.class);
+    }
+
+    @Bean
+    public UserOperationService userOperationService() {
+        return Mockito.mock(UserOperationService.class);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        return Mockito.mock(MessageSource.class);
+    }
+
+    @Bean
+    public OpenApiAuthService openApiAuthService() {
+        return Mockito.mock(OpenApiAuthService.class);
+    }
+
+    @Bean
+    public WalletService walletService() {
+        return Mockito.mock(WalletService.class);
+    }
+}
