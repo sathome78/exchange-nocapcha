88c5f936aa643584fcf7a2082812c8ef196433fe
Taranyk <grigoriytaranik@gmail.com>
2019-03-01 14:29:47 +0200

    Tian Tian Ventures - token ethereum

---
+++ Controller/src/main/resources/db/migration/V1.122__Ethereum_token_TTT.sql
@@ -0,0 +1,69 @@
+INSERT IGNORE INTO `MERCHANT` (`description`, `name`, `transaction_source_type_id`, `service_bean_name`, `process_type`, `tokens_parrent_id`)
+VALUES ('TTV', 'TTT', 2, 'ethereumServiceImpl', 'CRYPTO', 16);
+INSERT IGNORE INTO `CURRENCY` (`name`, `description`, `hidden`, `max_scale_for_refill`, `max_scale_for_withdraw`, `max_scale_for_transfer`)
+VALUES ('TTT', 'TTV', 0, 8, 8, 8);
+
+INSERT IGNORE INTO COMPANY_WALLET_EXTERNAL(currency_id) VALUES ((SELECT id from CURRENCY WHERE name='TTT'));
+
+
+INSERT IGNORE INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, refill_block, withdraw_block)
+  VALUES ((SELECT id from MERCHANT WHERE name='TTT'),
+          (SELECT id from CURRENCY WHERE name='TTT'),
+          0.00000001, TRUE, TRUE);
+
+INSERT IGNORE INTO `MERCHANT_IMAGE` (`merchant_id`, `image_path`, `image_name`, `currency_id`) VALUES ((SELECT id from MERCHANT WHERE name='TTT')
+, '/client/img/merchants/TTT.png', 'TTT', (SELECT id from CURRENCY WHERE name='TTT'));
+
+INSERT IGNORE INTO WALLET (user_id, currency_id) select id, (select id from CURRENCY where name='TTT') from USER;
+
+INSERT IGNORE INTO CURRENCY_LIMIT(currency_id, operation_type_id, user_role_id, min_sum, max_sum)
+  SELECT (select id from CURRENCY where name = 'TTT'), operation_type_id, user_role_id, min_sum, max_sum
+  FROM CURRENCY_LIMIT WHERE currency_id = (select id from CURRENCY where name = 'EDR');
+
+INSERT IGNORE INTO `COMPANY_WALLET` (`currency_id`) VALUES ((select id from CURRENCY where name = 'TTT'));
+
+INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
+VALUES((select id from CURRENCY where name = 'TTT'), (select id from CURRENCY where name = 'ETH'), 'TTT/ETH', 160, 0, 'ETH', 'TTT/ETH');
+
+INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
+  SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
+    JOIN USER_ROLE UR
+    JOIN ORDER_TYPE OT where CP.name='TTT/ETH';
+
+INSERT IGNORE INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, withdraw_block, refill_block, transfer_block)
+VALUES ((SELECT id FROM MERCHANT WHERE name = 'SimpleTransfer'), (select id from CURRENCY where name = 'TTT'), 0.000001, 1, 1, 0);
+
+INSERT IGNORE INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, withdraw_block, refill_block, transfer_block)
+VALUES ((SELECT id FROM MERCHANT WHERE name = 'VoucherTransfer'), (select id from CURRENCY where name = 'TTT'), 0.000001, 1, 1, 0);
+
+INSERT IGNORE INTO MERCHANT_CURRENCY (merchant_id, currency_id, min_sum, withdraw_block, refill_block, transfer_block)
+VALUES ((SELECT id FROM MERCHANT WHERE name = 'VoucherFreeTransfer'), (select id from CURRENCY where name = 'TTT'), 0.000001, 1, 1, 0);
+
+INSERT IGNORE INTO MERCHANT_IMAGE (merchant_id, image_path, image_name, currency_id) VALUES
+  ((SELECT id FROM MERCHANT WHERE name = 'SimpleTransfer'), '/client/img/merchants/transfer.png', 'Transfer', (select id from CURRENCY where name = 'TTT'));
+
+INSERT IGNORE INTO MERCHANT_IMAGE (merchant_id, image_path, image_name, currency_id) VALUES
+  ((SELECT id FROM MERCHANT WHERE name = 'VoucherTransfer'), '/client/img/merchants/voucher.png', 'Voucher', (select id from CURRENCY where name = 'TTT'));
+
+INSERT IGNORE INTO MERCHANT_IMAGE (merchant_id, image_path, image_name, currency_id) VALUES
+  ((SELECT id FROM MERCHANT WHERE name = 'VoucherFreeTransfer'), '/client/img/merchants/voucher_free.png', 'Free voucher', (select id from CURRENCY where name = 'TTT'));
+
+INSERT IGNORE INTO BOT_LAUNCH_SETTINGS(bot_trader_id, currency_pair_id)
+  SELECT BT.id, CP.id FROM BOT_TRADER BT
+    JOIN CURRENCY_PAIR CP WHERE CP.name IN ('TTT/ETH');
+
+INSERT IGNORE INTO BOT_TRADING_SETTINGS(bot_launch_settings_id, order_type_id)
+  SELECT BLCH.id, OT.id FROM BOT_LAUNCH_SETTINGS BLCH
+    JOIN ORDER_TYPE OT
+  WHERE BLCH.currency_pair_id IN (SELECT id FROM CURRENCY_PAIR WHERE name IN ('TTT/ETH'));
+
+INSERT IGNORE INTO INTERNAL_WALLET_BALANCES (currency_id, role_id)
+SELECT cur.id AS currency_id, ur.id AS role_id
+FROM CURRENCY cur CROSS JOIN USER_ROLE ur
+WHERE cur.name IN ('TTT')
+ORDER BY cur.id, ur.id;
+
+INSERT IGNORE INTO COMPANY_EXTERNAL_WALLET_BALANCES (currency_id)
+SELECT cur.id
+FROM CURRENCY cur
+WHERE cur.name IN ('TTT');
--- Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
+++ Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
@@ -10,11 +10,22 @@ public class TokenWrappersGenerator {
     public static void main(String[] args) throws Exception {
         SolidityFunctionWrapperGenerator.run(new String[]{
                 "generate",
-                "/Users/vlad.dziubak/crypto/eth/VRBS.bin",
-                "/Users/vlad.dziubak/crypto/eth/VRBS.abi",
+                "F:\\workspace\\exrates\\Controller\\src\\main\\resources/VRA.bin",
+                "F:\\workspace\\exrates\\Controller\\src\\main\\resources/VRA.abi",
                 "-o",
-                "/Users/vlad.dziubak/workspace/exrates/Service/src/main/java",
+                "F:\\workspace\\exrates\\Service\\src\\main\\java",
                 "-p",
                 "me.exrates.service.ethereum.ethTokensWrappers"});
+    }
+
+    public static void generateWrapper(String ticker, String filePathToBinAbiFiles, String filePathToWrappers, String wrappersPackage) throws Exception{
+        SolidityFunctionWrapperGenerator.run(new String[]{
+                "generate",
+                filePathToBinAbiFiles+ticker+".bin",
+                filePathToBinAbiFiles+ticker+".abi",
+                "-o",
+                filePathToWrappers,
+                "-p",
+                wrappersPackage});
     }
 }
