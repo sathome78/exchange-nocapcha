3800a326c3f63d7600fc2a73fe0555a610583ffd
maks11110 <avto12@i.ua>
2019-03-06 17:07:20 +0200

    modify migration scripts

--- Controller/src/main/resources/db/migration/V1.201902271124__alter_table_user_add_kyc_status_and_reference.sql
+++ Controller/src/main/resources/db/migration/V1.201902271124__alter_table_user_add_kyc_status_and_reference.sql
@@ -1,6 +1,37 @@
-ALTER TABLE USER
-  DROP COLUMN kyc_reference;
+DROP PROCEDURE IF EXISTS `Alter_Table`;
 
-ALTER TABLE USER
-  ADD COLUMN kyc_status VARCHAR(55) NOT NULL DEFAULT 'none',
-  ADD COLUMN kyc_reference VARCHAR(55);
+DELIMITER $$
+CREATE PROCEDURE `Alter_Table`()
+BEGIN
+
+    IF NOT EXISTS( SELECT NULL
+            FROM INFORMATION_SCHEMA.COLUMNS
+           WHERE table_name = 'USER'
+             AND table_schema = 'birzha'
+             AND column_name = 'kyc_reference')  THEN
+
+      ALTER TABLE `USER` DROP COLUMN kyc_reference;
+    END IF;
+
+    IF NOT EXISTS( SELECT NULL
+            FROM INFORMATION_SCHEMA.COLUMNS
+           WHERE table_name = 'USER'
+             AND table_schema = 'birzha'
+             AND column_name = 'kyc_status')  THEN
+
+      ALTER TABLE USER ADD COLUMN kyc_status VARCHAR(55) NOT NULL DEFAULT 'none';
+    END IF;
+
+    IF NOT EXISTS( SELECT NULL
+            FROM INFORMATION_SCHEMA.COLUMNS
+           WHERE table_name = 'USER'
+             AND table_schema = 'birzha'
+             AND column_name = 'kyc_reference')  THEN
+
+      ALTER TABLE USER ADD COLUMN kyc_reference VARCHAR(55);
+    END IF;
+
+END $$
+DELIMITER ;
+
+DROP PROCEDURE `Alter_Table`;
