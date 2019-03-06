7f978d2e57d8282d0d6440a3d05466daa18313a3
maks11110 <avto12@i.ua>
2019-03-06 12:37:54 +0200

    ws orderbook new

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -303,11 +303,11 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
         hikariConfig.setPassword(dbMasterPassword);
         hikariConfig.setMaximumPoolSize(50);
         DataSource dataSource = new HikariDataSource(hikariConfig);
-     /*   Flyway flyway = new Flyway();
+        Flyway flyway = new Flyway();
         flyway.setDataSource(dataSource);
         flyway.setBaselineOnMigrate(true);
         flyway.repair();
-        flyway.migrate();*/
+        flyway.migrate();
         return dataSource;
     }
 
