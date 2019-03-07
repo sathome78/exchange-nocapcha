912cf3ac74a9fa1e5c8ba093e26a9e2f264a40f9
StasGorobzeev <44207081+StasGorobzeev@users.noreply.github.com>
2019-03-07 13:59:38 +0200

    change doc api (#821)

--- Controller/src/main/java/me/exrates/controller/openAPI/OpenApiPublicController.java
+++ Controller/src/main/java/me/exrates/controller/openAPI/OpenApiPublicController.java
@@ -60,7 +60,7 @@ public class OpenApiPublicController {
      * @apiPosition 1
      * @api {get} /openapi/v1/public/ticker?currency_pair Ticker Info
      * @apiName Ticker
-     * @apiGroup 1_Public API
+     * @apiGroup Public API
      * @apiPermission user
      * @apiDescription Returns array of ticker info objects
      * @apiParam {String} currency_pair Currency pair name (optional)
@@ -108,7 +108,7 @@ public class OpenApiPublicController {
     /**
      * @api {get} /openapi/v1/public/orderbook/{currency_pair}?order_type Order Book
      * @apiName Order Book
-     * @apiGroup 1_Public API
+     * @apiGroup Public API
      * @apiPermission user
      * @apiDescription Books Order
      * @apiParam {String} order_type Order type (BUY or SELL) (optional)
@@ -129,7 +129,7 @@ public class OpenApiPublicController {
     /**
      * @api {get} /openapi/v1/public/history/{currency_pair}?from_date&to_date&limit&direction Trade History
      * @apiName Trade History
-     * @apiGroup 1_Public API
+     * @apiGroup Public API
      * @apiPermission user
      * @apiDescription Provides collection of trade info objects
      * @apiParam {LocalDate} from_date start date of search (date format: yyyy-MM-dd)
@@ -170,7 +170,7 @@ public class OpenApiPublicController {
     /**
      * @api {get} /openapi/v1/public/currency_pairs Currency Pairs
      * @apiName Currency Pairs
-     * @apiGroup 1_Public API
+     * @apiGroup Public API
      * @apiPermission user
      * @apiDescription Provides collection of currency pairs
      * @apiParamExample Request Example:
@@ -188,7 +188,7 @@ public class OpenApiPublicController {
     /**
      * @api {get} /openapi/v1/public/{currency_pair}/candle_chart?interval_type&interval_value Data for candle chart
      * @apiName Data for candle chart
-     * @apiGroup 1_Public API
+     * @apiGroup Public API
      * @apiPermission user
      * @apiDescription Data for candle chart
      * @apiParam {String} interval_type type of interval (valid values: "HOUR", "DAY", "MONTH", "YEAR")
