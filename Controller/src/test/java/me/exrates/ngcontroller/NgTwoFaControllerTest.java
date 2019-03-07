4e748e4b4524bf0ece2f6e75a6e24301fd791426
Andrew Zarazka <andreyzarazka@gmail.com>
2019-03-07 19:12:21 +0200

    created test class

---
+++ Controller/src/test/java/me/exrates/ngcontroller/NgTwoFaControllerTest.java
@@ -0,0 +1,33 @@
+package me.exrates.ngcontroller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NgTwoFaControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getSecurityCode() {
+    }
+
+    @Test
+    public void getSecurityPinCode() {
+    }
+
+    @Test
+    public void submitGoogleSecret() {
+    }
+
+    @Test
+    public void disableGoogleAuthentication() {
+    }
+
+    @Test
+    public void verifyGoogleAuthenticatorCode() {
+    }
+}
