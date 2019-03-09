8bb668b2f0f84b013fb15aa5e54f2f0cc59176e1
oleg <olegvanich@gmail.com>
2019-03-09 09:44:50 +0200

    fix migration issues

--- Controller/src/main/resources/db/migration/V1.128__add_field_scale_to_currency_table.sql
+++ Controller/src/main/resources/db/migration/V1.128__add_field_scale_to_currency_table.sql
@@ -1,33 +1,26 @@
 DROP PROCEDURE IF EXISTS `Alter_Table`;
 
-DELIMITER $$
-CREATE PROCEDURE `Alter_Table`()
+DELIMITER ';;'
+CREATE PROCEDURE Alter_Table()
   BEGIN
+    /* delete columns if they exist */
+    IF EXISTS (SELECT *
+               FROM information_schema.columns
+               WHERE table_name = 'CURRENCY' AND column_name = 'scale') THEN
 
-    IF EXISTS( SELECT NULL
-               FROM INFORMATION_SCHEMA.COLUMNS
-               WHERE table_name = 'CURRENCY'
-                     AND table_schema = 'birzha'
-                     AND column_name = 'scale')  THEN
-
-      ALTER TABLE CURRENCY DROP COLUMN scale;
+      ALTER TABLE CURRENCY DROP COLUMN `scale`;
     END IF;
 
-    IF NOT EXISTS( SELECT NULL
-                   FROM INFORMATION_SCHEMA.COLUMNS
-                   WHERE table_name = 'CURRENCY'
-                         AND table_schema = 'birzha'
-                         AND column_name = 'scale')  THEN
+    /* add columns */
+    ALTER TABLE CURRENCY ADD COLUMN scale INT(11) NULL;
 
-      ALTER TABLE CURRENCY ADD COLUMN scale INT(11) NULL;
-    END IF;
+  END;;
 
-  END $$
-DELIMITER ;
+DELIMITER ';'
 
 CALL Alter_Table();
 
-DROP PROCEDURE `Alter_Table`;
+DROP PROCEDURE IF EXISTS Alter_Table;
 
 UPDATE CURRENCY SET scale = 2 WHERE id IN (1, 2, 3, 7, 8, 10, 11, 12, 13, 17, 18, 20);
 UPDATE CURRENCY SET scale = 8 WHERE id NOT IN (1, 2, 3, 7, 8, 10, 11, 12, 13, 17, 18, 20);
