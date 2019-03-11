96e6d5acb00f0e9f5a84be5f6fed85919be7e645
Andrew Zarazka <andreyzarazka@gmail.com>
2019-03-11 22:56:10 +0200

    Tests failed: submitGoogleSecret_google_secret_invalid, submitGoogleSecret_google_secret_ok disableGoogleAuthentication

--- Controller/src/test/java/me/exrates/ngcontroller/AngularApiCommonTest.java
+++ Controller/src/test/java/me/exrates/ngcontroller/AngularApiCommonTest.java
@@ -1,17 +1,17 @@
 package me.exrates.ngcontroller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Sets;
 import me.exrates.security.service.AuthTokenService;
+import me.exrates.security.service.NgUserService;
 import me.exrates.service.UserService;
+import me.exrates.service.notifications.G2faService;
 import me.exrates.service.userOperation.UserOperationService;
 import org.junit.Before;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,18 +19,20 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.net.URI;
-import java.util.Collection;
-
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.when;
 
 public abstract class AngularApiCommonTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    NgUserService ngUserService;
 
     @Autowired
-    private AuthTokenService authTokenService;
+    G2faService g2faService;
+
+    @InjectMocks
+    NgTwoFaController ngTwoFaController;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
     @Autowired
     UserService userService;
@@ -40,17 +42,15 @@ public abstract class AngularApiCommonTest {
 
     MockMvc mockMvc;
 
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
     private final String HEADER_SECURITY_TOKEN = "Exrates-Rest-Token";
 
     @Before
     public void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
 
-        when(authTokenService.getUserByToken(anyString())).thenReturn(getTestUserDetails());
-        when(userService.getUserEmailFromSecurityContext()).thenReturn("test@test.me");
-        when(userService.getIdByEmail(anyString())).thenReturn(100);
-        when(userService.getPreferedLang(anyInt())).thenReturn("en");
-        when(userOperationService.getStatusAuthorityForUserByOperation(anyInt(), anyObject())).thenReturn(true);
     }
 
     protected RequestBuilder getApiRequestBuilder(URI uri, HttpMethod method, HttpHeaders httpHeaders, String content, String contentType) {
@@ -71,12 +71,6 @@ public abstract class AngularApiCommonTest {
         HttpHeaders headers = new HttpHeaders();
         headers.put(HEADER_SECURITY_TOKEN, ImmutableList.of("Test-Token"));
         return headers;
-    }
-
-    private UserDetails getTestUserDetails() {
-        Collection<GrantedAuthority> tokenPermissions = Sets.newHashSet();
-        return new User("name", "password", true,
-                false, false, false, tokenPermissions);
     }
 
 }
--- Controller/src/test/java/me/exrates/ngcontroller/NgTwoFaControllerTest.java
+++ Controller/src/test/java/me/exrates/ngcontroller/NgTwoFaControllerTest.java
@@ -1,14 +1,11 @@
 package me.exrates.ngcontroller;
 
+import me.exrates.model.User;
 import me.exrates.model.dto.Generic2faResponseDto;
 import me.exrates.security.config.ApiSecurityConfig;
-import me.exrates.security.service.NgUserService;
-import me.exrates.service.UserService;
-import me.exrates.service.notifications.G2faService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
@@ -19,8 +16,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -28,6 +23,9 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.handler.HandlerExceptionResolverComposite;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.anyInt;
@@ -42,33 +40,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class NgTwoFaControllerTest extends AngularApiCommonTest {
 
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private NgUserService ngUserService;
-
-    @Autowired
-    private G2faService g2faService;
-
-    @InjectMocks
-    private NgTwoFaController ngTwoFaController;
-
-    @Autowired
-    private WebApplicationContext wac;
-
-    private MockMvc mockMvc;
-
     private final String BASE_URL = "/api/private/v2/2FaOptions";
 
     @Before
     public void setUp() {
         ngTwoFaController = new NgTwoFaController(userService, g2faService, ngUserService);
 
-        SecurityContextHolder.getContext().setAuthentication(new AnonymousAuthenticationToken("GUEST","USERNAME", AuthorityUtils
-                .createAuthorityList("ROLE_ONE", "ROLE_TWO")));
-
-        HandlerExceptionResolver resolver = ((HandlerExceptionResolverComposite) wac
+        HandlerExceptionResolver resolver = ((HandlerExceptionResolverComposite) webApplicationContext
                 .getBean("handlerExceptionResolver"))
                 .getExceptionResolvers()
                 .get(0);
@@ -80,18 +58,14 @@ public class NgTwoFaControllerTest extends AngularApiCommonTest {
     }
 
     @Test
-    public void getSecurityCode_not_authorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/google2fa/hash")
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andDo(print())
-                .andExpect(status().isForbidden())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.errorCode", is("MISSING_AUTHENTICATION_TOKEN")));
-    }
-
-    @Test
     public void getSecurityCode() throws Exception {
+        SecurityContextHolder.getContext()
+                .setAuthentication(new AnonymousAuthenticationToken("GUEST", "USERNAME",
+                        AuthorityUtils.createAuthorityList("ROLE_ONE", "ROLE_TWO")));
+
         Generic2faResponseDto dto = new Generic2faResponseDto("message", "code", "error");
+
+        when(userService.getIdByEmail(anyString())).thenReturn(1);
 
         when(g2faService.getGoogleAuthenticatorCodeNg(anyInt())).thenReturn(dto);
 
@@ -113,78 +87,124 @@ public class NgTwoFaControllerTest extends AngularApiCommonTest {
     }
 
     @Test
-    public void getSecurityPinCode_not_authorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/google2fa/pin")
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andDo(print())
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
     public void getSecurityPinCode() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/google2fa/pin")
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
+        SecurityContextHolder.getContext()
+                .setAuthentication(new AnonymousAuthenticationToken("GUEST", "USERNAME",
+                        AuthorityUtils.createAuthorityList("ROLE_ONE", "ROLE_TWO")));
+
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .path(BASE_URL + "/google2fa/pin")
+                .build();
+
+        mockMvc.perform(getApiRequestBuilder(uriComponents.toUri(), HttpMethod.GET, null, "", MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andDo(print())
                 .andExpect(status().isOk());
 
-        verify(userService, times(1)).getIdByEmail(anyString());
+        verify(userService, times(1)).findByEmail(anyString());
 
         verify(g2faService, times(1)).sendGoogleAuthPinConfirm(anyObject(), anyObject());
     }
 
     @Test
-    public void submitGoogleSecret_not_authorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/google2fa/submit")
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
+    public void submitGoogleSecret_google_secret_invalid() throws Exception {
+        SecurityContextHolder.getContext()
+                .setAuthentication(new AnonymousAuthenticationToken("GUEST", "USERNAME",
+                        AuthorityUtils.createAuthorityList("ROLE_ONE", "ROLE_TWO")));
+
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .path(BASE_URL + "/google2fa/submit")
+                .build();
+
+        when(g2faService.submitGoogleSecret(anyObject(), anyObject())).thenReturn(Boolean.FALSE);
+
+        mockMvc.perform(getApiRequestBuilder(uriComponents.toUri(), HttpMethod.POST, null, "", MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andDo(print())
-                .andExpect(status().isForbidden());
+                .andExpect(status().isBadRequest());
+
+
+        verify(userService, times(0)).getIdByEmail(anyString());
+
+        verify(g2faService, times(0)).sendGoogleAuthPinConfirm(anyObject(), anyObject());
+
+        verify(ngUserService, times(0)).sendEmailDisable2Fa(anyString());
     }
 
     @Test
-    public void submitGoogleSecret_google_secret_invalid() throws Exception {
-        when(g2faService.submitGoogleSecret(anyObject(), anyObject())).thenReturn(Boolean.FALSE);
+    public void submitGoogleSecret_google_secret_ok() throws Exception {
+        SecurityContextHolder.getContext()
+                .setAuthentication(new AnonymousAuthenticationToken("GUEST", "USERNAME",
+                        AuthorityUtils.createAuthorityList("ROLE_ONE", "ROLE_TWO")));
 
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/google2fa/submit")
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .path(BASE_URL + "/google2fa/submit")
+                .build();
+
+        Map<String, String> map = new HashMap<>();
+        map.put(anyString(),anyString());
+
+        String body = objectMapper.writeValueAsString(map);
+
+        when(g2faService.submitGoogleSecret(any(User.class), anyMapOf(String.class,String.class))).thenReturn(Boolean.TRUE);
+
+        mockMvc.perform(getApiRequestBuilder(uriComponents.toUri(), HttpMethod.POST, null, body, MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andDo(print())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+
+
+        verify(userService, times(1)).getIdByEmail(anyString());
+
+        verify(g2faService, times(1)).sendGoogleAuthPinConfirm(anyObject(), anyObject());
+
+        verify(ngUserService, times(1)).sendEmailEnable2Fa(anyString());
+    }
+
+    @Test
+    public void disableGoogleAuthentication() throws Exception {
+    }
+
+    @Test
+    public void verifyGoogleAuthenticatorCode_isOk() throws Exception {
+        SecurityContextHolder.getContext()
+                .setAuthentication(new AnonymousAuthenticationToken("GUEST", "USERNAME",
+                        AuthorityUtils.createAuthorityList("ROLE_ONE", "ROLE_TWO")));
+
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .path(BASE_URL + "/verify_google2fa")
+                .queryParam("code", "CODE")
+                .build();
+
+        when(g2faService.checkGoogle2faVerifyCode(anyString(), anyInt())).thenReturn(Boolean.TRUE);
+
+        mockMvc.perform(getApiRequestBuilder(uriComponents.toUri(), HttpMethod.GET, null, "", MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+
+        verify(userService, times(1)).getIdByEmail(anyString());
+
+        verify(g2faService, times(1)).checkGoogle2faVerifyCode(anyString(), anyInt());
+    }
+
+    @Test
+    public void verifyGoogleAuthenticatorCode_invalid() throws Exception {
+        SecurityContextHolder.getContext()
+                .setAuthentication(new AnonymousAuthenticationToken("GUEST", "USERNAME",
+                        AuthorityUtils.createAuthorityList("ROLE_ONE", "ROLE_TWO")));
+
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+                .path(BASE_URL + "/verify_google2fa")
+                .queryParam("code", "CODE")
+                .build();
+
+        when(g2faService.checkGoogle2faVerifyCode(anyString(), anyInt())).thenReturn(Boolean.FALSE);
+
+        mockMvc.perform(getApiRequestBuilder(uriComponents.toUri(), HttpMethod.GET, null, "", MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andDo(print())
                 .andExpect(status().isBadRequest());
 
 
         verify(userService, times(1)).getIdByEmail(anyString());
 
-        verify(g2faService, times(1)).sendGoogleAuthPinConfirm(anyObject(), anyObject());
-    }
-
-    @Test
-    public void submitGoogleSecret_google_secret_ok() throws Exception {
-        when(g2faService.submitGoogleSecret(anyObject(), anyObject())).thenReturn(Boolean.TRUE);
-
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/google2fa/submit")
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andDo(print())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(status().isOk());
-
-
-        verify(userService, times(1)).getIdByEmail(anyString());
-
-        verify(g2faService, times(1)).sendGoogleAuthPinConfirm(anyObject(), anyObject());
-    }
-
-    @Test
-    public void disableGoogleAuthentication_not_authorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/google2fa/disable")
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andDo(print())
-                .andExpect(status().isForbidden());
-    }
-
-    @Test
-    public void verifyGoogleAuthenticatorCode_not_authorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/verify_google2fa")
-                .contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andDo(print())
-                .andExpect(status().isForbidden());
+        verify(g2faService, times(1)).checkGoogle2faVerifyCode(anyString(), anyInt());
     }
 }
