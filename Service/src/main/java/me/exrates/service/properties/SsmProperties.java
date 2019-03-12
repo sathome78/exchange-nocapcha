25d7b9080fc90e61186e9c8ed26ff2a2181cdbce
NMaslukov <JavaNoob14679987>
2019-03-12 14:59:20 +0200

    properties, ssm

--- Service/src/main/java/me/exrates/service/properties/SsmProperties.java
+++ Service/src/main/java/me/exrates/service/properties/SsmProperties.java
@@ -1,7 +1,6 @@
 package me.exrates.service.properties;
 
 import lombok.Data;
-import me.exrates.SSMGetter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -10,10 +9,8 @@ import org.springframework.stereotype.Component;
 @Data
 @PropertySource({"classpath:/ssm.properties"})
 public class SsmProperties {
-    private String inoutToken;
 
-    public SsmProperties(SSMGetter ssmGetter,
-                         @Value("${ssm.inout.api}") String inoutTokenPath) {
-        this.inoutToken = ssmGetter.lookup(inoutTokenPath);
-    }
+    @Value("${ssm.inout.api}")
+    private String inoutTokenPath;
+
 }
