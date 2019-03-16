7783e8b73de3452be50836db5ab785eceabc1398
andzarazka <48212351+andzarazka@users.noreply.github.com>
2019-03-16 18:07:14 +0200

    Devex 1522 language controller test (#858)
    * Fix method getUserLanguage(Principal principal), and covered with tests.
    
    * DEVEX-1522: LanguageControllerTest - all tests passed.
    
    * min refactor
    
    * Testing method getBalances
    
    * DEVEX-1523: Tests passed - method getBalances.
    
    Сommented out the @PostConstruct annotation on the private void init () method in the public class ExchangeRatesHolderImpl implements ExchangeRatesHolder, for building the project locally
    
    * DEVEX-1523: Tests passed - method getPendingRequests
    
    * DEVEX-1523: Tests passed - method revokeWithdrawRequest
    
    * DEVEX-1523: NgBalanceControllerTest - Tests passed all methods
    
    * fix angular common test conflict

--- Controller/src/test/java/me/exrates/ngcontroller/AngularApiCommonTest.java
+++ Controller/src/test/java/me/exrates/ngcontroller/AngularApiCommonTest.java
@@ -8,11 +8,16 @@ import me.exrates.model.CurrencyPair;
 import me.exrates.model.User;
 import me.exrates.model.dto.OrderBookWrapperDto;
 import me.exrates.model.dto.onlineTableDto.ExOrderStatisticsShortByPairsDto;
+import me.exrates.model.dto.onlineTableDto.MyInputOutputHistoryDto;
+import me.exrates.model.dto.onlineTableDto.MyWalletsDetailedDto;
+import me.exrates.model.dto.onlineTableDto.MyWalletsStatisticsDto;
 import me.exrates.model.dto.onlineTableDto.OrderAcceptedHistoryDto;
 import me.exrates.model.enums.CurrencyPairType;
 import me.exrates.model.enums.OperationType;
 import me.exrates.model.enums.OrderType;
+import me.exrates.model.enums.TransactionSourceType;
 import me.exrates.model.enums.UserStatus;
+import me.exrates.model.ngModel.RefillPendingRequestDto;
 import me.exrates.model.ngModel.ResponseInfoCurrencyPairDto;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -149,5 +154,98 @@ public abstract class AngularApiCommonTest {
         currency.setDescription("TEST_DESCRIPTION");
         currency.setHidden(Boolean.TRUE);
         return currency;
+    }
+    protected MyWalletsDetailedDto getMockMyWalletsDetailedDto() {
+        MyWalletsDetailedDto myWalletsDetailedDto = new MyWalletsDetailedDto();
+        myWalletsDetailedDto.setId(100);
+        myWalletsDetailedDto.setUserId(1);
+        myWalletsDetailedDto.setCurrencyId(111);
+        myWalletsDetailedDto.setCurrencyPrecision(222);
+        myWalletsDetailedDto.setCurrencyName("TEST_CURRENCY_NAME");
+        myWalletsDetailedDto.setCurrencyDescription("TEST_CURRENCY_DESCRIPTION");
+        myWalletsDetailedDto.setActiveBalance("TEST_ACTIVE_BALANCE");
+        myWalletsDetailedDto.setOnConfirmation("TEST_ON_CONFIRMATION");
+        myWalletsDetailedDto.setOnConfirmationStage("TEST_ON_CONFIRMATION_STAGE");
+        myWalletsDetailedDto.setOnConfirmationCount("TEST_ON_CONFIRMATION_COUNT");
+        myWalletsDetailedDto.setReservedBalance("TEST_RESERVED_BALANCE");
+        myWalletsDetailedDto.setReservedByOrders("TEST_RESERVED_BY_ORDERS");
+        myWalletsDetailedDto.setReservedByMerchant("TEST_RESERVED_BY_MERCHANT");
+        myWalletsDetailedDto.setBtcAmount("TEST_BTC_AMOUNT");
+        myWalletsDetailedDto.setUsdAmount("TEST_USD_AMOUNT");
+        myWalletsDetailedDto.setConfirmations(Collections.EMPTY_LIST);
+
+        return myWalletsDetailedDto;
+    }
+
+    protected RefillPendingRequestDto getMockRefillPendingRequestDto() {
+        return new RefillPendingRequestDto(777, "TEST_DATE", "TEST_CURRENCY", 100.0,
+                10.0, "TEST_SYSTEM", "TEST_STATUS", "TEST_OPERATION");
+    }
+
+    protected MyWalletsStatisticsDto getMockMyWalletsStatisticsDto(String currencyName) {
+        MyWalletsStatisticsDto myWalletsStatisticsDto = new MyWalletsStatisticsDto();
+        myWalletsStatisticsDto.setNeedRefresh(Boolean.TRUE);
+        myWalletsStatisticsDto.setPage(0);
+        myWalletsStatisticsDto.setDescription("TEST_DESCRIPTION");
+        myWalletsStatisticsDto.setCurrencyName(currencyName);
+        myWalletsStatisticsDto.setActiveBalance("TEST_ACTIVE_BALANCE");
+        myWalletsStatisticsDto.setTotalBalance("125");
+
+        return myWalletsStatisticsDto;
+    }
+
+    protected ExOrderStatisticsShortByPairsDto getMockExOrderStatisticsShortByPairsDto(String currencyPairName) {
+        ExOrderStatisticsShortByPairsDto exOrderStatisticsShortByPairsDto = new ExOrderStatisticsShortByPairsDto();
+        exOrderStatisticsShortByPairsDto.setNeedRefresh(Boolean.TRUE);
+        exOrderStatisticsShortByPairsDto.setPage(0);
+        exOrderStatisticsShortByPairsDto.setCurrencyPairId(222);
+        exOrderStatisticsShortByPairsDto.setCurrencyPairName(currencyPairName);
+        exOrderStatisticsShortByPairsDto.setCurrencyPairPrecision(22);
+        exOrderStatisticsShortByPairsDto.setLastOrderRate("22");
+        exOrderStatisticsShortByPairsDto.setPredLastOrderRate("");
+        exOrderStatisticsShortByPairsDto.setPercentChange("");
+        exOrderStatisticsShortByPairsDto.setMarket("");
+        exOrderStatisticsShortByPairsDto.setPriceInUSD("");
+        exOrderStatisticsShortByPairsDto.setType(CurrencyPairType.MAIN);
+        exOrderStatisticsShortByPairsDto.setVolume("");
+        exOrderStatisticsShortByPairsDto.setCurrencyVolume("");
+        exOrderStatisticsShortByPairsDto.setHigh24hr("");
+        exOrderStatisticsShortByPairsDto.setLow24hr("");
+        exOrderStatisticsShortByPairsDto.setLastUpdateCache("");
+
+        return exOrderStatisticsShortByPairsDto;
+    }
+
+    protected MyInputOutputHistoryDto getMockMyInputOutputHistoryDto() {
+        MyInputOutputHistoryDto myInputOutputHistoryDto = new MyInputOutputHistoryDto();
+        myInputOutputHistoryDto.setDatetime(LocalDateTime.of(2019, 03, 13, 14, 00, 00));
+        myInputOutputHistoryDto.setCurrencyName("TEST_CURRENCY_NAME");
+        myInputOutputHistoryDto.setAmount("TEST_AMOUNT");
+        myInputOutputHistoryDto.setCommissionAmount("TEST_COMMISSION_AMOUNT");
+        myInputOutputHistoryDto.setMerchantName("TEST_MERCHANT_NAME");
+        myInputOutputHistoryDto.setOperationType("TEST_OPERATION_TYPE");
+        myInputOutputHistoryDto.setTransactionId(100);
+        myInputOutputHistoryDto.setProvided(200);
+        myInputOutputHistoryDto.setTransactionProvided("TEST_TRANSACTION_PROVIDED");
+        myInputOutputHistoryDto.setId(300);
+        myInputOutputHistoryDto.setDestination("TEST_DESCRIPTION");
+        myInputOutputHistoryDto.setUserId(400);
+        myInputOutputHistoryDto.setBankAccount("TEST_BANK_ACCOUNT");
+        myInputOutputHistoryDto.setStatusUpdateDate(LocalDateTime.of(2019, 03, 14, 15, 00, 00));
+        myInputOutputHistoryDto.setSummaryStatus("TEST_SUMMARY_STATUS");
+        myInputOutputHistoryDto.setUserFullName("TEST_USER_FULL_NAME");
+        myInputOutputHistoryDto.setRemark("TEST_REMARK");
+        myInputOutputHistoryDto.setSourceId(TransactionSourceType.REFILL.toString());
+        myInputOutputHistoryDto.setSourceType(TransactionSourceType.REFILL);
+        myInputOutputHistoryDto.setConfirmation(700);
+        myInputOutputHistoryDto.setNeededConfirmations(800);
+        myInputOutputHistoryDto.setAdminHolderId(900);
+        myInputOutputHistoryDto.setAuthorisedUserId(1000);
+        myInputOutputHistoryDto.setButtons(Collections.EMPTY_LIST);
+        myInputOutputHistoryDto.setTransactionHash("TEST_TRANSACTIONAL_HASH");
+        myInputOutputHistoryDto.setMarket("TEST_MARKET");
+        myInputOutputHistoryDto.setAccepted(Boolean.TRUE);
+
+        return myInputOutputHistoryDto;
     }
 }
