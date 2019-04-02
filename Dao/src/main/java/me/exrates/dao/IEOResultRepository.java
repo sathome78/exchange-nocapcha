605dad14c13ca7608ebb0e533bd0f23bbc70e45b
olegpodolian <oleg.podolian@upholding.biz>
2019-04-02 14:56:00 +0300

    add IEOQueueServiceImpl validation

--- Dao/src/main/java/me/exrates/dao/IEOResultRepository.java
+++ Dao/src/main/java/me/exrates/dao/IEOResultRepository.java
@@ -1,10 +1,15 @@
 package me.exrates.dao;
 
+import me.exrates.model.IEOClaim;
 import me.exrates.model.IEOResult;
+
+import java.math.BigDecimal;
 
 public interface IEOResultRepository {
 
     IEOResult create(IEOResult ieoResult);
 
     boolean updateStatus(int id, IEOResult.IEOResultStatus status);
+
+    BigDecimal getAvailableBalance(IEOClaim ieoClaim);
 }
--- Service/src/main/java/me/exrates/service/ieo/impl/IEOQueueServiceImpl.java
+++ Service/src/main/java/me/exrates/service/ieo/impl/IEOQueueServiceImpl.java
@@ -1,6 +1,7 @@
 package me.exrates.service.ieo.impl;
 
 import lombok.extern.log4j.Log4j2;
+import me.exrates.dao.IEOResultRepository;
 import me.exrates.model.IEOClaim;
 import me.exrates.model.Wallet;
 import me.exrates.model.constants.ErrorApiTitles;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -20,12 +22,15 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class IEOQueueServiceImpl implements IEOQueueService {
 
     private final Queue<IEOClaim> claims;
+    private final IEOResultRepository ieoResultRepository;
     private final UserService userService;
     private final WalletService walletService;
 
     @Autowired
-    public IEOQueueServiceImpl(UserService userService,
+    public IEOQueueServiceImpl(IEOResultRepository ieoResultRepository,
+                               UserService userService,
                                WalletService walletService) {
+        this.ieoResultRepository = ieoResultRepository;
         this.walletService = walletService;
         this.claims = new ConcurrentLinkedQueue<>();
         this.userService = userService;
@@ -38,14 +43,28 @@ public class IEOQueueServiceImpl implements IEOQueueService {
 
     @Override
     public boolean add(IEOClaim claim) {
+        validateClaimIntent(claim);
+
+
+        return false;
+    }
+
+    private void validateClaimIntent(IEOClaim claim) {
         Wallet userWallet = walletService.findByUserAndCurrency(claim.getUserId(), "BTC");
-        if (userWallet == null || userWallet.getActiveBalance().compareTo(claim.getPriceInBtc()) > 0) {
-            // user has insufficient funds
+        if (userWallet == null || userWallet.getActiveBalance().compareTo(claim.getPriceInBtc()) < 0) {
             String message = String.format("Failed to apply as user has insufficient funds: suggested %s BTC, but available is %s BTC",
                     claim.getPriceInBtc().toPlainString(), userWallet == null ? "0" : userWallet.getActiveBalance().toPlainString());
             log.warn(message);
             throw new IeoException(ErrorApiTitles.IEO_INSUFFICIENT_BUYER_FUNDS, message);
         }
-        return false;
+        // or we should split it ???
+        BigDecimal availableBalance = ieoResultRepository.getAvailableBalance(claim);
+        if (availableBalance.compareTo(claim.getAmount()) < 0) {
+            String message = String.format("Failed to accept as ieo provider has insufficient funds: queried %s, but available is %s BTC",
+                    claim.getAmount().toPlainString(), availableBalance);
+            log.warn(message);
+            throw new IeoException(ErrorApiTitles.IEO_INSUFFICIENT_AVAILABLE_FUNDS, message);
+        }
     }
+
 }
