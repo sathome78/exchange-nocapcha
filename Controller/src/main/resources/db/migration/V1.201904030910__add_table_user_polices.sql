c71ce7d9286e8d3d4a81d46fb968012ceff123e7
StasGorobzeev <44207081+StasGorobzeev@users.noreply.github.com>
2019-04-05 09:02:15 +0300

    Ieo initi (#1127)
    * add info
    
    * add ieoClaim, ieoResult
    
    * add ioe claim to db
    
    * add country to kyc
    
    * add IEOQueueServiceImpl
    
    * add IEOQueueServiceImpl validation
    
    * add IEOQueueServiceImpl block
    
    * add IEOQueueServiceImpl fix
    
    * add IEO Processor
    
    * fix ieo claim
    
    * add IEO Processor 2
    
    * change create claim
    
    * add findUnprocessedIeoClaims in dao
    
    * add exception handler
    
    * fix ieo details
    
    * fix ieo repositories
    
    * fix ieo service
    
    * fix ieo service 1
    
    * add kyc check
    
    * Merge branch 'ieo_initi' of https://github.com/roadtomoon/exrates into ieo_initi
    
    # Conflicts:
    #	Dao/src/main/java/me/exrates/dao/UserDao.java
    #	Dao/src/main/java/me/exrates/dao/impl/IEOClaimRepositoryImpl.java
    #	Dao/src/main/java/me/exrates/dao/impl/UserDaoImpl.java
    #	Model/src/main/java/me/exrates/model/dto/ieo/IEOStatusInfo.java
    #	Service/src/main/java/me/exrates/service/UserService.java
    #	Service/src/main/java/me/exrates/service/impl/IEOServiceImpl.java
    #	Service/src/main/java/me/exrates/service/impl/UserServiceImpl.java
    
    * add apiDoc
    
    * fix ieo service 2
    
    * find one
    
    * and send
    
    * added user rtestrictions
    
    * fix launch errors
    
    * fix select user sql
    
    * some fixing
    
    * admin ieo
    
    * some fixing
    
    * some fixing 2
    
    * some fixing 3
    
    * some logs
    
    * some fixing 5
    
    * some exception handling
    
    * added transaction handling
    
    * added fix 455
    
    * admin ieo
    
    * added check if started
    
    * fix class name
    
    * admin ieo
    
    * fix transaction
    
    * fix notification
    
    * fix notification 1
    
    * fix merge
    
    * fix test
    
    * fix sql migration
    
    * fix sql migration
    
    * fix sql migration

--- Controller/src/main/resources/db/migration/V1.201904030910__add_table_user_polices.sql
+++ Controller/src/main/resources/db/migration/V1.201904030910__add_table_user_polices.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS USER_POLICES
   id        INT(11) PRIMARY KEY         NOT NULL AUTO_INCREMENT,
   policy_id INT(11)                     NOT NULL,
   user_id   INT(11)                     NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES User (id),
+  FOREIGN KEY (user_id) REFERENCES USER (id),
   FOREIGN KEY (policy_id) REFERENCES POLICY (id)
 )
   ENGINE = INNODB;
