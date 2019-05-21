0609c70a7f94c6118c2b6f1e40a50847229ea2ed
StasGorobzeev <stas.gorobzeev@tech-stack.io>
2019-05-21 14:33:15 +0300

    change migration

--- Controller/src/main/resources/db/migration/V1.201905183460__alter_table_ieo_claim_result_add_message_column.sql
+++ Controller/src/main/resources/db/migration/V1.201905183460__alter_table_ieo_claim_result_add_message_column.sql
@@ -10,19 +10,6 @@ CREATE PROCEDURE Alter_Table()
       ALTER TABLE IEO_RESULT ADD COLUMN message VARCHAR(255) NULL;
     END IF;
 
-  END;;
-
-DELIMITER ;
-
-CALL Alter_Table();
-
-DROP PROCEDURE IF EXISTS Alter_Table;
-
-DROP PROCEDURE IF EXISTS `Alter_Table`;
-
-DELIMITER ;;
-CREATE PROCEDURE Alter_Table()
-  BEGIN
     IF NOT EXISTS( SELECT NULL
                    FROM INFORMATION_SCHEMA.COLUMNS
                    WHERE table_name = 'IEO_CLAIM' AND column_name = 'uuid')  THEN
@@ -30,25 +17,11 @@ CREATE PROCEDURE Alter_Table()
       ALTER TABLE IEO_CLAIM ADD COLUMN uuid VARCHAR(64) NOT NULL;
     END IF;
 
-  END;;
-
-DELIMITER ;
-
-CALL Alter_Table();
-
-DROP PROCEDURE IF EXISTS Alter_Table;
-
-DROP PROCEDURE IF EXISTS `Alter_Table`;
-
-DELIMITER ;;
-CREATE PROCEDURE Alter_Table()
-  BEGIN
     IF NOT EXISTS( SELECT NULL
                    FROM INFORMATION_SCHEMA.COLUMNS
                    WHERE table_name = 'IEO_CLAIM' AND column_name = 'test')  THEN
 
-      ALTER TABLE IEO_CLAIM ADD COLUMN test TINYINT(1) NOT NULL;
-    END IF;
+      ALTER
 
   END;;
 
--- Controller/src/main/resources/db/migration/V1.201905192257__alter_table_ieo_details_add_sold_out_column.sql
+++ Controller/src/main/resources/db/migration/V1.201905192257__alter_table_ieo_details_add_sold_out_column.sql
@@ -10,19 +10,6 @@ CREATE PROCEDURE Alter_Table()
       ALTER TABLE IEO_DETAILS ADD COLUMN sold_out_at timestamp NULL;
     END IF;
 
-  END;;
-
-DELIMITER ;
-
-CALL Alter_Table();
-
-DROP PROCEDURE IF EXISTS Alter_Table;
-
-DROP PROCEDURE IF EXISTS `Alter_Table`;
-
-DELIMITER ;;
-CREATE PROCEDURE Alter_Table()
-  BEGIN
     IF NOT EXISTS( SELECT NULL
                    FROM INFORMATION_SCHEMA.COLUMNS
                    WHERE table_name = 'IEO_DETAILS' AND column_name = 'test_ieo')  THEN
@@ -30,23 +17,11 @@ CREATE PROCEDURE Alter_Table()
       ALTER TABLE IEO_DETAILS ADD COLUMN test_ieo TINYINT(1) NULL;
     END IF;
 
-  END;;
-
-DELIMITER ;
-
-CALL Alter_Table();
-
-DROP PROCEDURE IF EXISTS Alter_Table;
-
-
-DELIMITER ;;
-CREATE PROCEDURE Alter_Table()
-  BEGIN
     IF NOT EXISTS( SELECT NULL
                    FROM INFORMATION_SCHEMA.COLUMNS
                    WHERE table_name = 'IEO_DETAILS' AND column_name = 'count_test_transaction')  THEN
 
-      ALTER TABLE IEO_DETAILS ADD COLUMN count_test_transaction int(11) NULL;
+      ALTER TABLE IEO_DETAILS ADD COLUMN count_test_transaction int(11) DEFAULT 20 NULL;
     END IF;
 
   END;;
@@ -56,6 +31,7 @@ DELIMITER ;
 CALL Alter_Table();
 
 DROP PROCEDURE IF EXISTS Alter_Table;
+
 
 INSERT IGNORE INTO USER (id, email) VALUES (-2, 'test_ieo@gmail.com');
 
