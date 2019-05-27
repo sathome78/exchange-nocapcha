2eef651b902ad9199235de4adef18f5de003c982
gregory.taranik <gregory.taranik@upholding.biz>
2019-05-27 18:14:02 +0300

    TRX/BTT

--- Controller/src/main/resources/db/migration/V1.201905271356__add_currency_pair_trx-btt.sql
+++ Controller/src/main/resources/db/migration/V1.201905271356__add_currency_pair_trx-btt.sql
@@ -1,5 +1,5 @@
-INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, ticker_name)
-VALUES((select id from CURRENCY where name = 'TRX'), (select id from CURRENCY where name = 'BTT'), 'TRX/BTT', 170, 0, 'TRX/BTT');
+INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, ticker_name, market)
+VALUES((select id from CURRENCY where name = 'TRX'), (select id from CURRENCY where name = 'BTT'), 'TRX/BTT', 160, 0, 'TRX/BTT', 'FIAT');
 
 INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
   SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
