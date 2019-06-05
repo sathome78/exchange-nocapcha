2ed9c2e5a4568abee9ace16b81ac9e9863d1951d
gregory.taranik <gregory.taranik@upholding.biz>
2019-06-05 18:41:52 +0300

    Up

--- Service/src/main/java/me/exrates/service/usdx/model/UsdxTransaction.java
+++ Service/src/main/java/me/exrates/service/usdx/model/UsdxTransaction.java
@@ -1,8 +1,6 @@
 package me.exrates.service.usdx.model;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
