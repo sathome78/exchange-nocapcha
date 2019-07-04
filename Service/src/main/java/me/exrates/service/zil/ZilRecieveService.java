07fec8ac2da445f11b4484eee2f8f9a81815149e
gregory.taranik <gregory.taranik@upholding.biz>
2019-07-04 16:44:09 +0300

    temp

--- Service/src/main/java/me/exrates/service/zil/ZilRecieveService.java
+++ Service/src/main/java/me/exrates/service/zil/ZilRecieveService.java
@@ -1,4 +1,11 @@
 package me.exrates.service.zil;
 
+import com.google.gson.Gson;
+
 public class ZilRecieveService {
+    public static void main(String[] args) {
+        HttpProvider client = new HttpProvider("https://api.zilliqa.com/");
+        Rep<DsBlock> dsBlock = client.getLatestDsBlock();
+        System.out.println(new Gson().toJson(dsBlock));
+    }
 }
