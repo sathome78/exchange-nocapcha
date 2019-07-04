5e8616e338a8914de3ecf82258f73019f80e05bb
gregory.taranik <gregory.taranik@upholding.biz>
2019-07-04 17:08:10 +0300

    test Zil RPC method

--- Service/src/main/java/me/exrates/service/zil/ZilRecieveService.java
+++ Service/src/main/java/me/exrates/service/zil/ZilRecieveService.java
@@ -1,9 +1,15 @@
 package me.exrates.service.zil;
 
+import com.firestack.laksaj.blockchain.DsBlock;
+import com.firestack.laksaj.exception.ZilliqaAPIException;
+import com.firestack.laksaj.jsonrpc.HttpProvider;
+import com.firestack.laksaj.jsonrpc.Rep;
 import com.google.gson.Gson;
 
+import java.io.IOException;
+
 public class ZilRecieveService {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ZilliqaAPIException {
         HttpProvider client = new HttpProvider("https://api.zilliqa.com/");
         Rep<DsBlock> dsBlock = client.getLatestDsBlock();
         System.out.println(new Gson().toJson(dsBlock));
