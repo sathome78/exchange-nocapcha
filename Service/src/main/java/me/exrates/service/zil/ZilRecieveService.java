86cd1e152340a8f211182d86b89982530f6ae1d3
gregory.taranik <gregory.taranik@upholding.biz>
2019-07-04 18:17:35 +0300

    test Zil up

--- Service/src/main/java/me/exrates/service/zil/ZilRecieveService.java
+++ Service/src/main/java/me/exrates/service/zil/ZilRecieveService.java
@@ -4,14 +4,34 @@ import com.firestack.laksaj.blockchain.DsBlock;
 import com.firestack.laksaj.exception.ZilliqaAPIException;
 import com.firestack.laksaj.jsonrpc.HttpProvider;
 import com.firestack.laksaj.jsonrpc.Rep;
-import com.google.gson.Gson;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 public class ZilRecieveService {
+
+    private static HttpProvider client;
+
+    @PostConstruct
+    private void init(){
+        client = new HttpProvider("https://api.zilliqa.com/");
+    }
+
     public static void main(String[] args) throws IOException, ZilliqaAPIException {
-        HttpProvider client = new HttpProvider("https://api.zilliqa.com/");
+        new ZilRecieveService().init();
         Rep<DsBlock> dsBlock = client.getLatestDsBlock();
-        System.out.println(new Gson().toJson(dsBlock));
+        System.out.println(dsBlock);
+    }
+
+    int getLastBlock(){
+        return 0;
+    }
+
+    int getLastBlockNum(){
+        return 0;
+    }
+
+    void saveLastBlock(){
+
     }
 }
--- Service/src/main/java/me/exrates/service/zil/ZilServiceImpl.java
+++ Service/src/main/java/me/exrates/service/zil/ZilServiceImpl.java
@@ -1,4 +1,41 @@
 package me.exrates.service.zil;
 
+import me.exrates.model.Currency;
+import me.exrates.model.Merchant;
+import me.exrates.model.condition.MonolitConditional;
+import me.exrates.service.CurrencyService;
+import me.exrates.service.MerchantService;
+import me.exrates.service.RefillService;
+import me.exrates.service.util.WithdrawUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+@Conditional(MonolitConditional.class)
 public class ZilServiceImpl {
+
+    private static final String CURRENCY_NAME = "ZIL";
+    private Merchant merchant;
+    private Currency currency;
+
+    @Autowired
+    private CurrencyService currencyService;
+    @Autowired
+    private MerchantService merchantService;
+    @Autowired
+    private RefillService refillService;
+    @Autowired
+    private MessageSource messageSource;
+    @Autowired
+    private WithdrawUtils withdrawUtils;
+
+    @PostConstruct
+    public void init() {
+        currency = currencyService.findByName(CURRENCY_NAME);
+        merchant = merchantService.findByName(CURRENCY_NAME);
+    }
 }
