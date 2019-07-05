ad0be10f9a95f9da43b1f0fb29214a1d1bf43c75
gregory.taranik <gregory.taranik@upholding.biz>
2019-07-05 11:39:03 +0300

    temp tests

--- Service/src/main/java/me/exrates/service/zil/ZilRecieveService.java
+++ Service/src/main/java/me/exrates/service/zil/ZilRecieveService.java
@@ -1,11 +1,12 @@
 package me.exrates.service.zil;
 
+import com.firestack.laksaj.blockchain.BlockList;
 import com.firestack.laksaj.blockchain.BlockchainInfo;
-import com.firestack.laksaj.blockchain.DsBlock;
 import com.firestack.laksaj.blockchain.TxBlock;
 import com.firestack.laksaj.exception.ZilliqaAPIException;
 import com.firestack.laksaj.jsonrpc.HttpProvider;
 import com.firestack.laksaj.jsonrpc.Rep;
+import com.google.gson.Gson;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -20,16 +21,33 @@ public class ZilRecieveService {
     }
 
     public static void main(String[] args) throws IOException, ZilliqaAPIException {
-        new ZilRecieveService().init();
-        Rep<DsBlock> dsBlock = client.getLatestDsBlock();
-        Rep<String> numDSBlocks = client.getNumDSBlocks();
-        Rep<BlockchainInfo> blockchainInfo = client.getBlockchainInfo();
-        Rep<TxBlock> txBlock = client.getLatestTxBlock();
-        System.out.println(txBlock);
+        ZilRecieveService zilRecieveService = new ZilRecieveService();
+        zilRecieveService.init();
+//        zilRecieveService.checkRefills();
+
+
+        Rep<TxBlock> txBlock = client.getTxBlock("160590");
+//        Rep<TxBlock> txBlock = client.getLatestTxBlock();
+        Rep<String> numTxBlocks = client.getNumTxBlocks();
+        Rep<Double> txBlockRate = client.getTxBlockRate();
+        Rep<BlockList> blockListing = client.getTxBlockListing(5);
+        System.out.println(new Gson().toJson(txBlock));
+        System.out.println(new Gson().toJson(blockListing));
     }
 
-    int getLastBlock(){
+    void checkRefills(){
+        int lastblock = getLastBaseBlock();
+        int lastNum = getLastBlockNum();
+        System.out.println(lastblock + "    " + lastNum);
+
+    }
+
+    int getLastBaseBlock(){
         return 0;
+    }
+
+    void getTxBlock(){
+
     }
 
     int getLastBlockNum(){
