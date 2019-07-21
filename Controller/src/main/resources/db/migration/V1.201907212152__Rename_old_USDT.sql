464c7a0254713a38090d7cde83a20c2d6990b758
vlad.dziubak <vlad.dziubak@upholding.biz>
2019-07-21 22:35:33 +0300

    USDT - change to ERC20

---
+++ Controller/src/main/resources/db/migration/V1.201907212152__Rename_old_USDT.sql
@@ -0,0 +1,45 @@
+UPDATE `MERCHANT` SET name ='USDT-old' WHERE id=(SELECT id FROM `MERCHANT` WHERE name = 'USDT');
+
+UPDATE `CURRENCY` SET name ='USDT-old' WHERE id=(SELECT id FROM `CURRENCY` WHERE name = 'USDT');
+
+UPDATE `CURRENCY_PAIR` SET name = 'BTC/USDT-old', hidden = 1, ticker_name = 'BTC/USDT-old' WHERE name = 'BTC/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'ETH/USDT-old', hidden = 1, ticker_name = 'ETH/USDT-old' WHERE name = 'ETH/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'LTC/USDT-old', hidden = 1, ticker_name = 'LTC/USDT-old' WHERE name = 'LTC/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'ETC/USDT-old', hidden = 1, ticker_name = 'ETC/USDT-old' WHERE name = 'ETC/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'DASH/USDT-old', hidden = 1, ticker_name = 'DASH/USDT-old' WHERE name = 'DASH/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'XRP/USDT-old', hidden = 1, ticker_name = 'XRP/USDT-old' WHERE name = 'XRP/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'XLM/USDT-old', hidden = 1, ticker_name = 'XLM/USDT-old' WHERE name = 'XLM/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'XEM/USDT-old', hidden = 1, ticker_name = 'XEM/USDT-old' WHERE name = 'XEM/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'ATB/USDT-old', hidden = 1, ticker_name = 'ATB/USDT-old' WHERE name = 'ATB/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'EDC/USDT-old', hidden = 1, ticker_name = 'EDC/USDT-old' WHERE name = 'EDC/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'IOTA/USDT-old', hidden = 1, ticker_name = 'IOTA/USDT-old' WHERE name = 'IOTA/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'DOGE/USDT-old', hidden = 1, ticker_name = 'DOGE/USDT-old' WHERE name = 'DOGE/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'LSK/USDT-old', hidden = 1, ticker_name = 'LSK/USDT-old' WHERE name = 'LSK/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'XMR/USDT-old', hidden = 1, ticker_name = 'XMR/USDT-old' WHERE name = 'XMR/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'NEO/USDT-old', hidden = 1, ticker_name = 'NEO/USDT-old' WHERE name = 'NEO/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'GAS/USDT-old', hidden = 1, ticker_name = 'GAS/USDT-old' WHERE name = 'GAS/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'ZEC/USDT-old', hidden = 1, ticker_name = 'ZEC/USDT-old' WHERE name = 'ZEC/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'WAVES/USDT-old', hidden = 1, ticker_name = 'WAVES/USDT-old' WHERE name = 'WAVES/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'BNB/USDT-old', hidden = 1, ticker_name = 'BNB/USDT-old' WHERE name = 'BNB/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'VRBS/USDT-old', hidden = 1, ticker_name = 'VRBS/USDT-old' WHERE name = 'VRBS/USDT';
+
+UPDATE `CURRENCY_PAIR` SET name = 'CRON/USDT-old', hidden = 1, ticker_name = 'CRON/USDT-old' WHERE name = 'CRON/USDT';
