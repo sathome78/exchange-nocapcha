6f9d3537ea041a5a0f715baf8c34b4b70647e5a4
Maxim Peresadko <maxim.peresadko@upholding.biz>
2019-08-07 13:19:27 +0200

    Merge pull request #182 in MAIN/exrates from DEVEX-3353-add-volumes-for-markets to staging
    Squashed commit of the following:
    
    commit 7f3bbace78b6b90194a6b7d00a2a6d4673bf5f2f
    Author: StasGorobzeev <stanislav.gorobzeev@upholding.biz>
    Date:   Tue Aug 6 15:04:15 2019 +0300
    
        add new market volumes

---
+++ Controller/src/main/resources/db/migration/V1.201908061003__insert_market_volume.sql
@@ -0,0 +1,13 @@
+INSERT IGNORE INTO CURRENCY_PAIR_MARKET_VOLUMES(name, market_volume)
+VALUES ('TUSD', 5000),
+       ('USDC', 5000),
+       ('EUR', 4500),
+       ('RUB', 300000),
+       ('UAH', 125000),
+       ('VND', 100000000),
+       ('CNY', 35000),
+       ('IDR', 70000000),
+       ('NGN', 25000),
+       ('TRY', 18000),
+       ('AED', 1500000),
+       ('KGS', 300000);
