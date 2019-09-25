e2b156d1109e821b94221d6ccccbca409c721ea8
olegpodolian <oleg.podolian@upholding.biz>
2019-09-25 23:14:38 +0300

    fix NPE for ieo

--- Service/src/main/java/me/exrates/service/impl/WalletServiceImpl.java
+++ Service/src/main/java/me/exrates/service/impl/WalletServiceImpl.java
@@ -86,6 +86,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -868,7 +869,9 @@ public class WalletServiceImpl implements WalletService {
             userIeoWallet = walletDao.createWallet(ieoClaim.getUserId(), currencyId);
         }
 
-        BigDecimal makerBtcInitialAmount = makerBtcWallet.getIeoReserved();
+        BigDecimal makerBtcInitialAmount = Objects.isNull(makerBtcWallet.getIeoReserved())
+                ? ZERO
+                : makerBtcWallet.getIeoReserved();
         makerBtcWallet.setIeoReserved(makerBtcInitialAmount.add(ieoClaim.getPriceInBtc()));
 
         BigDecimal updateActiveUserBalanceBtc = userBtcWallet.getActiveBalance().subtract(ieoClaim.getPriceInBtc());
