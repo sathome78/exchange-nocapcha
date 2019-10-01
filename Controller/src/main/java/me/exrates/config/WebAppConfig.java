1c8a82451852f39a5b47e239b283605c4fa3b0e7
Gregory Taranik <gregory.taranik@upholding.biz>
2019-10-01 09:29:19 +0200

    Merge pull request #350 in MAIN/exrates from DEVEX-4114__new_contract_1irstcoin_FST to staging
    Squashed commit of the following:
    
    commit c7594958bebc063809ca1ab66b366f3f22e65793
    Author: gregory.taranik <gregory.taranik@upholding.biz>
    Date:   Mon Sep 30 12:21:22 2019 +0300
    
        up
    
    commit 4132fcc8513a0af31aa537abfa6128773d378e7a
    Merge: f9d8ca603 e11c3645e
    Author: gregory.taranik <gregory.taranik@upholding.biz>
    Date:   Mon Sep 30 12:06:47 2019 +0300
    
        Merge remote-tracking branch 'origin/staging' into staging
    
        # Conflicts:
        #	Controller/src/main/resources/messages_ar.properties
        #	Controller/src/main/webapp/client/js/freecoinsDistributionList.js
        #	Service/src/main/java/me/exrates/service/freecoins/FreecoinsServiceImpl.java
    
    commit f9d8ca603af44bbf16e18986054c55fdc7f88b8f
    Author: Oleg Kostiukevych <oleg.kostiukevych@upholding.biz>
    Date:   Wed Sep 25 13:20:12 2019 +0200
    
        Merge pull request #329 in MAIN/exrates from DEVEX-4036 to staging
    
        Squashed commit of the following:
    
        commit 2a76082e5babfec70e6011dfd010eea80f8bef8f
        Author: olegkostiukevych <oleg.kostiukevych@upholding.biz>
        Date:   Mon Sep 23 05:28:13 2019 +0300
    
            DEVEX-4036 (freecoins admin panel)

--- Controller/src/main/java/me/exrates/config/WebAppConfig.java
+++ Controller/src/main/java/me/exrates/config/WebAppConfig.java
@@ -2038,7 +2038,7 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
     @Conditional(MonolitConditional.class)
     public EthTokenService fstServiceImpl() {
         List<String> tokensList = new ArrayList<>();
-        tokensList.add("0xa1a6f16d26aa53aec17e4001fd8cb6e6d5b17ff7");
+        tokensList.add("0x310c93dfc1c5e34cdf51678103f63c41762089cd");
         return new EthTokenServiceImpl(tokensList, "FST", "FST", true, ExConvert.Unit.MWEI);
     }
 
