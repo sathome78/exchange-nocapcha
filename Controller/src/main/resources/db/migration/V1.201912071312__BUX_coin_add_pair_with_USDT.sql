8f4d9ed1c232e430efb7f1c15f36f102d78a40de
Vlad Dziubak <vlad.dziubak@upholding.biz>
2019-12-09 23:09:05 +0100

    Merge pull request #576 in MAIN/exrates from DEVEX-4612-coin-bux-usdt to integration
    Squashed commit of the following:
    
    commit 46ecf093143fabe5a9fb6bb61a8550a93a490bd5
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Sat Dec 7 13:17:51 2019 +0200
    
        DEVEX-4612 - добавление торговой пары BUX/USDT

---
+++ Controller/src/main/resources/db/migration/V1.201912071312__BUX_coin_add_pair_with_USDT.sql
@@ -0,0 +1,16 @@
+INSERT IGNORE INTO CURRENCY_PAIR (currency1_id, currency2_id, name, pair_order, hidden, market ,ticker_name)
+VALUES((select id from CURRENCY where name = 'BUX'), (select id from CURRENCY where name = 'USDT'), 'BUX/USDT', 170, 0, 'USDT', 'BUX/USDT');
+
+INSERT IGNORE INTO CURRENCY_PAIR_LIMIT (currency_pair_id, user_role_id, order_type_id, min_rate, max_rate)
+  SELECT CP.id, UR.id, OT.id, 0, 99999999999 FROM CURRENCY_PAIR CP
+    JOIN USER_ROLE UR
+    JOIN ORDER_TYPE OT where CP.name='BUX/USDT';
+
+INSERT IGNORE INTO BOT_LAUNCH_SETTINGS(bot_trader_id, currency_pair_id)
+  SELECT BT.id, CP.id FROM BOT_TRADER BT
+    JOIN CURRENCY_PAIR CP WHERE CP.name IN ('BUX/USDT');
+
+INSERT IGNORE INTO BOT_TRADING_SETTINGS(bot_launch_settings_id, order_type_id)
+  SELECT BLCH.id, OT.id FROM BOT_LAUNCH_SETTINGS BLCH
+    JOIN ORDER_TYPE OT
+  WHERE BLCH.currency_pair_id IN (SELECT id FROM CURRENCY_PAIR WHERE name IN ('BUX/USDT'));
