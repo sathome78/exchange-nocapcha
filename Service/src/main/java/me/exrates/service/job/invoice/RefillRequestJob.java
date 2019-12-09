9563bc7e1fafdad39662c7b490b390f7f2afb07c
Vlad Dziubak <vlad.dziubak@upholding.biz>
2019-12-09 23:08:51 +0100

    Merge pull request #577 in MAIN/exrates from feature/DEVEX-4566-coin-eureka-coin-erk to integration
    Squashed commit of the following:
    
    commit f118e3a6135ba8899d33c75aa75016dc2b44ff94
    Merge: 47d211f3d1 4c6e7d8cdb
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Tue Dec 10 00:03:01 2019 +0200
    
        Merge branch 'feature/DEVEX-4566-coin-eureka-coin-erk' into integration
    
        # Conflicts:
        #	Controller/src/main/prod/node_config/node_config_bux.properties
    
    commit 47d211f3d1d230a4c964f08a008ea345f987cc19
    Merge: 075c031451 a57ee9af03
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Tue Dec 10 00:02:27 2019 +0200
    
        Merge remote-tracking branch 'origin/integration' into integration
    
    commit 4c6e7d8cdbe422d5dd93b9b79a71478e13d48671
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Tue Dec 10 00:01:51 2019 +0200
    
        ERK - add to RefillRequestJob
    
    commit e1b40e81722a675bdb373ab970432965c987ae45
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Mon Dec 9 00:55:50 2019 +0200
    
        ERK - фикс по портам для дева и прода
    
    commit f1cb9990ece1ff4e3d2553860aa521e782cbda4a
    Merge: c2918f6876 075c031451
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Thu Dec 5 00:36:41 2019 +0200
    
        Merge branch 'integration' into feature/DEVEX-4566-coin-eureka-coin-erk
    
        # Conflicts:
        #	Controller/src/main/java/me/exrates/config/BtcGenerator.java
        #	Controller/src/main/java/me/exrates/config/CryptocurrencyConfig.java
    
    commit c2918f6876878133cd8ed63765bfe49b7fa09c5d
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Thu Dec 5 00:35:51 2019 +0200
    
        DEVEX-4566 - добавление биткоин подобной монеты Erkcoin (ERK)
    
        Изменение торговой пары (вместо usd - usdt)
    
    commit 075c031451406a73f6ad5857fed5620e55a49570
    Merge: 8b0520a994 5a802f5d4a
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Thu Dec 5 00:16:23 2019 +0200
    
        Merge remote-tracking branch 'origin/integration' into integration
    
    commit 8b0520a9943028aa7442e20b0eb0a22cb19286df
    Merge: c3e22b3d25 bbe4f2de0d
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Sun Dec 1 13:15:11 2019 +0200
    
        Merge branch 'DEVEX-4570-coin-buxcoin-bux' into integration
    
    commit bbe4f2de0daca8fe5a29b6f38a1ab0d13ead2bd4
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Sun Dec 1 13:11:51 2019 +0200
    
        DEVEX-4570 - добавление биткоин подобной монеты Buxcoin (BUX)
    
    commit c3e22b3d257464fb4875e44038f56410552c86d9
    Merge: 6c023a282e 33a3d39a09
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Sat Nov 30 14:18:16 2019 +0200
    
        Merge remote-tracking branch 'origin/integration' into integration
    
    commit 9eb964e31168f8174cb0ffe1b6d0565d2261bae3
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Tue Nov 26 23:53:51 2019 +0200
    
        DEVEX-4566 - подключение (без картинки)
    
    commit 6c023a282ed9c382286b060fba56ed445f4b5eb0
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Sun Nov 3 21:53:21 2019 +0200
    
        DEVEX-4290 - переподключение эфирного токена MEXC

--- Service/src/main/java/me/exrates/service/job/invoice/RefillRequestJob.java
+++ Service/src/main/java/me/exrates/service/job/invoice/RefillRequestJob.java
@@ -50,7 +50,7 @@ public class RefillRequestJob {
 
         log.info("Starting refillCheckPaymentsForCoins");
         String[] merchantNames = new String[]{"QRK", "LBTC", "LPC", "XFC", "DDX", "MBC", "BTCP", "CLX", "ABBC",
-                "CBC", "BTCZ", "KOD", "RIME", "DIVI", "OWC", "VOLLAR", "WOLF", "TSL", "DOGE", "EST", "FGC", "BUX"};
+                "CBC", "BTCZ", "KOD", "RIME", "DIVI", "OWC", "VOLLAR", "WOLF", "TSL", "DOGE", "EST", "FGC", "BUX", "ERK"};
         for (String coin : merchantNames) {
             try {
                 getBitcoinServiceByMerchantName(coin).scanForUnprocessedTransactions(null);
