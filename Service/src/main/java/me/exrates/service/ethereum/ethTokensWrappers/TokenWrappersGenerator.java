e02efbc7730ebeadfece731aa74abb47954b1648
Oleg Podolyan <oleg.podolyan@upholding.biz>
2019-12-12 10:04:26 +0100

    Merge pull request #580 in MAIN/exrates from feature/DEVEX-4616-coin-birdchain-bird-from-master to master
    Squashed commit of the following:
    
    commit 6c5fc821fa17f18c2d36e55c8064206651675729
    Author: vlad.dziubak <vlad.dziubak@upholding.biz>
    Date:   Wed Dec 11 22:08:34 2019 +0200
    
        DEVEX-4616 - подключение эфирного токена Birdchain (BIRD)

--- Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
+++ Service/src/main/java/me/exrates/service/ethereum/ethTokensWrappers/TokenWrappersGenerator.java
@@ -10,8 +10,8 @@ public class TokenWrappersGenerator {
     public static void main(String[] args) throws Exception {
         SolidityFunctionWrapperGenerator.run(new String[]{
                 "generate",
-                "/Users/vlad.dziubak/crypto/eth/MEXC.bin",
-                "/Users/vlad.dziubak/crypto/eth/MEXC.abi",
+                "/Users/vlad.dziubak/crypto/eth/BIRD.bin",
+                "/Users/vlad.dziubak/crypto/eth/BIRD.abi",
                 "-o",
                 "/Users/vlad.dziubak/workspace/exrates/Service/src/main/java",
                 "-p",
